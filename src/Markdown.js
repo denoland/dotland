f771e276b92fd546a6e03f65a698c9504d430aa8
Ryan Dahl <ry@tinyclouds.org>
2019-10-19 05:55:11 -0400

    Registry can render code now
    Try http://localhost:3000/std/http/http_status.ts

--- src/Markdown.js
+++ src/Markdown.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "./CodeBlock";
 
+const renderers = { code: CodeBlock };
+
 function Markdown(props) {
-  return (
-    <ReactMarkdown source={props.source} renderers={{ code: CodeBlock }} />
-  );
+  return <ReactMarkdown source={props.source} renderers={renderers} />;
 }
 
 export default Markdown;
