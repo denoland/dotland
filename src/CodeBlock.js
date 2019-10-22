2eb094f12389c0eee88bfe31ebaee3baf160f8d7
Ry Dahl <ry@tinyclouds.org>
2019-10-22 04:55:59 -0400

    web design (#19)

--- src/CodeBlock.js
+++ src/CodeBlock.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
+import theme from "react-syntax-highlighter/dist/esm/styles/hljs/github-gist";
 
 function CodeBlock(props) {
   return (
-    <SyntaxHighlighter language={props.language || "js"}>
+    <SyntaxHighlighter style={theme} language={props.language || "js"}>
       {props.value}
     </SyntaxHighlighter>
   );
