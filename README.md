5775d424c2cbda1319de4cb1bb45e8b7be8fff51
Ry Dahl <ry@tinyclouds.org>
2019-10-22 04:20:49 -0400

    list directories for /std/ and /x/ (#18)

--- README.md
+++ README.md
@@ -2,15 +2,8 @@ This is a rewrite of the Deno website it will combine the code in
 https://github.com/denoland/deno/tree/master/website
 and https://github.com/denoland/registry and have faster deployment.
 
-This is written in React / MaterialUI / Netlify. Not in Deno. Ideally this could be
-ported to Deno at some point but we are in need of a new website and dogfooding
-takes too long. We hope to see this code ported to Deno with minimal developer
-flow interrupted (in particular, we need the ability to listen for FS events and
-reload the web server).
-
-## TODO
-
-- [x] style guide
-- [ ] benchmarks
-- [ ] /x/
-- [ ] plaintext code when curl /std/http/server.ts
+This is written in React / MaterialUI / Netlify / CloudFlare Workers. Not in
+Deno. Ideally this could be ported to Deno at some point but we are in need of a
+new website and dogfooding takes too long. We hope to see this code ported to
+Deno with minimal developer flow interrupted (in particular, we need the ability
+to listen for FS events and reload the web server).
--- src/Registry.js
+++ src/Registry.js
@@ -1,34 +1,61 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Markdown from "./Markdown";
 import CodeBlock from "./CodeBlock";
 import { proxy } from "./registry_utils";
 
 export default function Registry(params) {
-  const [state, setState] = React.useState({ contents: "loading", rUrl: null });
+  const [state, setState] = React.useState({
+    contents: "loading",
+    rUrl: null,
+    dir: null
+  });
   const location = useLocation();
 
   React.useEffect(
     () => {
       const { pathname } = location;
       let { entry, path } = proxy(pathname);
-      if (!path) {
-        path = "README.md";
+      console.log({ path });
+      if (!path || path.endsWith("/")) {
+        // Render dir.
+        renderDir(path, entry).then(dir => {
+          console.log({ dir });
+          setState({ dir });
+        });
+      } else {
+        // Render file.
+        const rUrl = `${entry.url}${path}`;
+        console.log("fetch", rUrl);
+        fetch(rUrl).then(async response => {
+          const m = await response.text();
+          setState({ contents: m, rUrl });
+        });
       }
-      const rUrl = `${entry.url}${path}`;
-      console.log("fetch", rUrl);
-      fetch(rUrl).then(async response => {
-        const m = await response.text();
-        setState({ contents: m, rUrl });
-      });
     },
     [location]
   );
 
   let contentComponent;
-  if (state.rUrl) {
-    if (state.rUrl.endsWith(".md")) {
+  if (state.dir) {
+    const entries = [];
+    for (let d of state.dir) {
+      const hasExtension = d.name.indexOf(".") >= 1;
+      const name = hasExtension ? d.name : d.name + "/";
+      entries.push(
+        <tr>
+          <td>{d.type}</td>
+          <td>{d.size}</td>
+          <td>
+            <Link to={name}>{name}</Link>
+          </td>
+        </tr>
+      );
+    }
+    contentComponent = <table>{entries}</table>;
+  } else {
+    if (state.rUrl && state.rUrl.endsWith(".md")) {
       contentComponent = <Markdown source={state.contents} />;
     } else {
       // TODO(ry) pass language to CodeBlock.
@@ -37,4 +64,43 @@ export default function Registry(params) {
   }
 
   return <Box>{contentComponent}</Box>;
+}
+
+async function renderDir(pathname, entry) {
+  console.log({ pathname, entry });
+  if (entry.raw.type === "github") {
+    const url = `https://api.github.com/repos/${entry.raw.owner}/${
+      entry.raw.repo
+    }/contents/${entry.raw.path}${pathname}?ref=${entry.branch}`;
+    console.log("renderDir", url);
+    const res = await fetch(url, {
+      headers: {
+        //authorization:
+        //  process.env.GH_TOKEN && "token " + process.env.GH_TOKEN,
+        accept: "application/vnd.github.v3.object"
+      }
+    });
+    if (res.status !== 200) {
+      throw Error(
+        `Got an error (${
+          res.status
+        }) when querying the GitHub API:\n${await res.text()}`
+      );
+    }
+    const data = await res.json();
+    if (data.type !== "dir") {
+      throw Error(
+        `Unexpected type ${
+          data.type
+        } when querying the GitHub API:\n${JSON.stringify(data, null, 2)}`
+      );
+    }
+
+    return data.entries.map(entry => ({
+      name: entry.name,
+      type: entry.type, // "file" | "dir" | "symlink"
+      size: entry.size, // file only
+      target: entry.target // symlink only
+    }));
+  }
 }
