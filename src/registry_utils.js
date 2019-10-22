13e861a9152fb5db21b127b14ad840f9ca8692da
Ryan Dahl <ry@tinyclouds.org>
2019-10-22 18:04:52 -0400

    Backwards compatibility with deno_std

--- src/registry_utils.js
+++ src/registry_utils.js
@@ -36,6 +36,13 @@ export function proxy(pathname) {
  * @return {import('./types').Entry}
  */
 export function getEntry(name, branch = "master") {
+  // denoland/deno_std was merged into denoland/deno. For a while we will try
+  // to maintain old links for backwards compatibility with denoland/deno_std
+  // but eventually tags before v0.20.0 will break.
+  if (name == "std" && !(branch == "master" || branch == "v0.21.0")) {
+    name = "std_old";
+  }
+
   const rawEntry = DATABASE[name];
   if (!rawEntry) {
     return null;
