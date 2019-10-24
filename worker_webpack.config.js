9706cd137fa42d757f817541ff6e0c39e35e7bcb
Ry Dahl <ry@tinyclouds.org>
2019-10-24 16:48:24 -0400

    Publish cloudflare worker in Github Actions (#37)

---
+++ worker_webpack.config.js
@@ -0,0 +1,8 @@
+// This is for the CloudFlare worker. Ideally this configuration could live in
+// the worker sub-directory but it seems the github actions deployment doesn't
+// like that.
+module.exports = {
+  target: 'webworker',
+  entry: './worker/index.js',
+  mode: 'production',
+}
