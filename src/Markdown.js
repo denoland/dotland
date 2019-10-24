fa4eb8c97dfeee57bb9edf8e35e8db7f8bd92e37
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-10-24 14:20:32 +0530

    Fix TOC

--- src/Markdown.js
+++ src/Markdown.js
@@ -2,7 +2,33 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "./CodeBlock";
 
-const renderers = { code: CodeBlock };
+function flatten(text, child) {
+  return typeof child === "string"
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text);
+}
+
+// Using logic from markdown-toc
+const slugify = text => {
+  text = text.toLowerCase();
+  text = text.split(" ").join("-");
+  text = text.split(/\t/).join("--");
+  text = text.split(/[|$&`~=\\/@+*!?({[\]})<>=.,;:'"^]/).join("");
+  text = text
+    .split(/[。？！，、；：“”【】（）〔〕［］﹃﹄“ ”‘’﹁﹂—…－～《》〈〉「」]/)
+    .join("");
+
+  return text;
+};
+
+function HeadingRenderer(props) {
+  var children = React.Children.toArray(props.children);
+  var text = children.reduce(flatten, "");
+  var slug = slugify(text);
+  return React.createElement("h" + props.level, { id: slug }, props.children);
+}
+
+const renderers = { code: CodeBlock, heading: HeadingRenderer };
 
 function Markdown(props) {
   return <ReactMarkdown source={props.source} renderers={renderers} />;
