ce6e26a8f2f0e3e92a85671169bcd96a7522a672
Ryan Dahl <ry@tinyclouds.org>
2019-10-24 16:41:09 -0400

    clean up

--- worker_webpack.config.js
+++ worker_webpack.config.js
@@ -1,3 +1,6 @@
+// This is for the CloudFlare worker. Ideally this configuration could live in
+// the worker sub-directory but it seems the github actions deployment doesn't
+// like that.
 module.exports = {
   target: 'webworker',
   entry: './worker/index.js',
