290d56f52bd2021e9321ecefec6d82202b10ba7c
Kevin (Kun) Kassimo Qian <kevinkassimo@gmail.com>
2019-10-24 22:49:24 -0700

    Add basic doc sidebar

--- src/Docs.tsx
+++ src/Docs.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { main } from "./doc_utils";
 import CodeBlock from "./CodeBlock";
 import { Link, useLocation } from "react-router-dom";
 import {
-  List,
   ListItem,
   Box,
-  Button,
   Card,
   CardHeader,
-  CardContent
+  CardContent,
+  Drawer,
+  Button,
 } from "@material-ui/core";
+import "./Docs.scss";
 
 interface Props {
   source: string;
@@ -19,33 +20,46 @@ interface Props {
 export default function Docs(props: Props) {
   const location = useLocation();
   const docs = main(location.pathname, props.source);
+  const [drawerOpen, setDrawerOpen] = useState(true);
 
   return (
-    <div>
-      <nav>
-        <List>
-          {docs.map(d => {
+    <div className="docs">
+      <nav className="sidebar">
+        <Drawer
+          variant="persistent"
+          anchor={'left'}
+          open={drawerOpen}
+          onClose={setDrawerOpen.bind(null, false)}
+        >
+          <div className="header">
+            <Button onClick={setDrawerOpen.bind(null, false)}>Close</Button>
+            <h2>Menu</h2>
+          </div>
+          {docs.map((d, i) => {
             return (
-              <ListItem>
+              <ListItem key={i}>
                 <a href={`?doc#${d.name}`}>{d.name}</a>
               </ListItem>
             );
           })}
-        </List>
+        </Drawer>
       </nav>
-      {docs.map(d => {
-        return (
-          <Box my={3} id={d.name}>
-            <Card raised={true}>
-              <CardHeader title={d.name} subheader={d.typestr} />
-              <CardContent>
-                <p>{d.docstr}</p>
-                <CodeBlock language="json" value={JSON.stringify(d, null, 1)} />
-              </CardContent>
-            </Card>
-          </Box>
-        );
-      })}
+      <main>
+        {<Button onClick={() => setDrawerOpen(!drawerOpen)}>{drawerOpen ? "Hide Menu" : "Open Menu"}</Button>}
+        {docs.map(d => {
+          return (
+            <Box my={3} id={d.name}>
+              <Card raised={true}>
+                <CardHeader title={d.name} subheader={d.typestr} />
+                <CardContent>
+                  <p>{d.docstr}</p>
+                  <CodeBlock language="json" value={JSON.stringify(d, null, 1)} />
+                </CardContent>
+              </Card>
+            </Box>
+          );
+        })}
+      </main>
     </div>
   );
 }
