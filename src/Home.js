1f95aaa6f25b231d4240b5191780abcca5a34fb3
Andy Hayden <andyhayden1@gmail.com>
2019-10-28 11:45:49 -0700

    Use for-await in home page example

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,14 @@ import { InternalLink } from "./InternalLink";
 import "./Home.css";
 import { Link } from "@material-ui/core";
 
-const code = `import { serve } from "https://deno.land/std@v0.21.0/http/server.ts";
+const code = `import { serve } from "https://deno.land/std@v0.22.0/http/server.ts";
 const body = new TextEncoder().encode("Hello World\\n");
 const s = serve(":8000");
-window.onload = async () => {
-  console.log("http://localhost:8000/");
-  for await (const req of s) {
-    req.respond({ body });
-  }
-};`;
+console.log("http://localhost:8000/");
+for await (const req of s) {
+  req.respond({ body });
+}
+`;
 
 function Home() {
   return (
