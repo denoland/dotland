f458717d1cc348d0100833908da6f5ea25ab8daa
josephsintum <sjumbam15@gmail.com>
2019-10-30 18:47:36 -0700

    adding Link interface

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { HashLink, HashLinkProps } from "react-router-hash-link";
 import { Link as MaterialLink } from "@material-ui/core";
+import { LinkProps } from "@material-ui/core/Link/Link";
 
 const InternalLink = React.forwardRef((props: HashLinkProps, ref) => (
   <HashLink {...props} />
 ));
 
-export const Link = props => {
+interface Props {
+  /** Supports both internal and external links */
+  to: string;
+  /** Do not use href. Use <Link to="link" > instead for both internal and external links */
+  href?: never;
+}
+
+export const Link: React.FC<Props & LinkProps> = props => {
   if (props.to.indexOf("://") === -1) {
     return <MaterialLink component={InternalLink} {...props} />;
   } else {
