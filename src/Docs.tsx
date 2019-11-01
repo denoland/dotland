785ae8af4a5385581979715c20c7b6c691dccf9b
Ry Dahl <ry@tinyclouds.org>
2019-11-01 01:51:06 -0400

    Add sidebar to docs (#100)

--- src/Docs.tsx
+++ src/Docs.tsx
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 import Link from "./Link";
 import {
   List,
+  Grid,
   ListItem,
   ListItemText,
   Box,
@@ -22,81 +23,92 @@ export default function Docs(props: Props) {
   const docs = main(location.pathname, props.source);
 
   return (
-    <div>
-      <nav>
-        <List>
-          {docs.map(d => {
-            return (
-              <ListItem key={d.name}>
-                <Link to={`?doc#${d.name}`}>{d.name}</Link>
-              </ListItem>
+    <Grid
+      container
+      zeroMinWidth
+      spacing={3}
+      direction="row"
+      justify="flex-start"
+      alignItems="flex-start"
+    >
+      <Grid item>
+        <nav>
+          <List>
+            {docs.map(d => {
+              return (
+                <ListItem key={d.name}>
+                  <Link to={`?doc#${d.name}`}>{d.name}</Link>
+                </ListItem>
+              );
+            })}
+          </List>
+        </nav>
+      </Grid>
+      <Grid item>
+        {docs.map(d => {
+          const href = "?doc#" + d.name;
+          const title = (
+            <Link to={href} color="inherit">
+              <code>{d.name}</code>
+            </Link>
+          );
+          const subheader = (
+            <span>
+              {d.kind} <code>{d.typestr}</code>
+            </span>
+          );
+          let frag = location.hash.substr(1);
+          const raised = frag === d.name;
+          let docstr = null;
+          if (d.docstr) {
+            docstr = <Markdown source={d.docstr} />;
+          }
+          let args = null;
+          if (d.args) {
+            args = (
+              <p>
+                <b>Arguments</b>
+                <List>
+                  {d.args.map(arg => {
+                    let name = <code>{`${arg.name}: ${arg.typestr}`}</code>;
+                    let docstr = arg.docstr ? (
+                      <Markdown source={arg.docstr} />
+                    ) : null;
+                    return (
+                      <ListItem>
+                        <ListItemText primary={name} secondary={docstr} />
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </p>
             );
-          })}
-        </List>
-      </nav>
-      {docs.map(d => {
-        const href = "?doc#" + d.name;
-        const title = (
-          <Link to={href} color="inherit">
-            <code>{d.name}</code>
-          </Link>
-        );
-        const subheader = (
-          <span>
-            {d.kind} <code>{d.typestr}</code>
-          </span>
-        );
-        let frag = location.hash.substr(1);
-        const raised = frag === d.name;
-        let docstr = null;
-        if (d.docstr) {
-          docstr = <Markdown source={d.docstr} />;
-        }
-        let args = null;
-        if (d.args) {
-          args = (
-            <p>
-              <b>Arguments</b>
-              <List>
-                {d.args.map(arg => {
-                  let name = <code>{`${arg.name}: ${arg.typestr}`}</code>;
-                  let docstr = arg.docstr ? (
-                    <Markdown source={arg.docstr} />
-                  ) : null;
-                  return (
-                    <ListItem>
-                      <ListItemText primary={name} secondary={docstr} />
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </p>
+          }
+          let ret = null;
+          if (d.retType) {
+            ret = (
+              <p>
+                <b>Return Type</b> {d.retType}
+              </p>
+            );
+          }
+          return (
+            <Box key={d.name} my={3} id={d.name}>
+              <Card raised={raised}>
+                <CardHeader title={title} subheader={subheader} />
+                <CardContent>
+                  {docstr}
+                  {args}
+                  {ret}
+                  {/*
+                    <CodeBlock language="json" value={JSON.stringify(d, null, 1)} />
+                    */}
+                </CardContent>
+              </Card>
+            </Box>
           );
-        }
-        let ret = null;
-        if (d.retType) {
-          ret = (
-            <p>
-              <b>Return Type</b> {d.retType}
-            </p>
-          );
-        }
-        return (
-          <Box key={d.name} my={3} id={d.name}>
-            <Card raised={raised}>
-              <CardHeader title={title} subheader={subheader} />
-              <CardContent>
-                {docstr}
-                {args}
-                {ret}
-                {/*
-                  <CodeBlock language="json" value={JSON.stringify(d, null, 1)} />
-                  */}
-              </CardContent>
-            </Card>
-          </Box>
-        );
-      })}
-    </div>
+        })}
+      </Grid>
+    </Grid>
   );
 }
