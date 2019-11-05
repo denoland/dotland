096e468263c18d0aba97a840410617cb66f46991
Ryan Dahl <ry@tinyclouds.org>
2019-11-05 12:41:38 -0500

    Remove custom colors

--- src/Benchmarks.tsx
+++ src/Benchmarks.tsx
@@ -39,22 +39,7 @@ function BenchmarkChart(props: Props) {
   const options = {
     theme: {
       mode: theme.palette.type,
-      palette: "palette1"
     },
-    colors: [
-      "#a6cee3",
-      "#1f78b4",
-      "#b2df8a",
-      "#33a02c",
-      "#fb9a99",
-      "#e31a1c",
-      "#fdbf6f",
-      "#ff7f00",
-      "#cab2d6",
-      "#6a3d9a",
-      "#ffff99",
-      "#b15928"
-    ],
     chart: {
       background: theme.palette.background.default,
       foreColor: theme.palette.getContrastText(
