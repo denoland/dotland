ec602e59d7fcd80b9ad3cb22488bd42c540ee354
Ry Dahl <ry@tinyclouds.org>
2019-11-15 00:15:33 -0500

    Improve code example on home page (#115)

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import CodeBlock from "./CodeBlock";
 import Link from "./Link";
 import "./Home.css";
 
-const code = `import { serve } from "https://deno.land/std@v0.22.0/http/server.ts";
+const code = `import { serve } from "https://deno.land/std@v0.24.0/http/server.ts";
 const body = new TextEncoder().encode("Hello World\\n");
-const s = serve(":8000");
+const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
   req.respond({ body });
