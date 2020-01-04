c5184f0d3647e3fc9e1b27784e4d0a2250243d74
Sameh Fakoua <s.fakoua@gmail.com>
2020-01-04 15:55:03 +0200

    Remove unused import

--- src/page/RegistryIndex.js
+++ src/page/RegistryIndex.js
@@ -5,7 +5,6 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
-import Paper from '@material-ui/core/Paper';
 
 export default function RegistryIndex() {
   return (
