34e63879297d4ab6741e7112dd4d27b95b87072d
Ryan Dahl <ry@tinyclouds.org>
2020-01-04 21:32:32 -0500

    Revert "Add description field to database.json (#160)"
    This reverts commit 7107b386bcadd2bb21f1fb875ea3e2d4627bb154.

--- src/page/RegistryIndex.js
+++ src/page/RegistryIndex.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Link from "../component/Link";
 import DATABASE from "../database.json";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
 
 export default function RegistryIndex() {
   return (
@@ -45,28 +41,19 @@ export default function RegistryIndex() {
       </p>
 
       <p>{Object.entries(DATABASE).length} third party modules:</p>
-      <List dense>
+
+      <ul>
         {Object.keys(DATABASE)
           .sort((nameA, nameB) => nameA.localeCompare(nameB))
           .map((name, i) => {
             const link = `/x/${name}/`;
             return (
-              <React.Fragment key={i}>
-              <ListItem alignItems="flex-start">
-                <ListItemText
-                  primary={<Link href={link}>{name}</Link>}
-                  secondary={
-                    <React.Fragment>
-                      {DATABASE[name].desc}
-                    </React.Fragment>
-                  }
-                />
-              </ListItem>
-              <Divider/>
-              </React.Fragment>
+              <li key={i}>
+                <Link href={link}>{name}</Link>
+              </li>
             );
           })}
-      </List>
+      </ul>
     </main>
   );
 }
