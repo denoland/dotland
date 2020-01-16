bd25abe8cc8d91cd85eae418fef4b49b84e02fd2
Sameh Fakoua <s.fakoua@gmail.com>
2020-01-17 00:21:50 +0200

    Add GUI for copy code cell (#181)

--- src/component/CodeBlock.tsx
+++ src/component/CodeBlock.tsx
@@ -11,6 +11,11 @@ import json from "react-syntax-highlighter/dist/cjs/languages/hljs/json";
 import markdown from "react-syntax-highlighter/dist/cjs/languages/hljs/markdown";
 import shell from "react-syntax-highlighter/dist/cjs/languages/hljs/shell";
 import { useDarkMode } from "../hook/theme";
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import CheckIcon from '@material-ui/icons/Check';
+import { IconButton, Container, Zoom } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 lightTheme["hljs-selection"] = {
   backgroundColor: "#ebebeb" // https://github.com/atom/one-light-ui/blob/master/styles/ui-variables.less#L32
@@ -19,6 +24,24 @@ darkTheme["hljs-selection"] = {
   backgroundColor: "#3a404b" // https://github.com/atom/one-dark-ui/blob/master/styles/ui-variables.less#L32
 };
 
+const useStyles = makeStyles(theme => ({
+  copyButton: {
+    position: 'relative',
+    right: '5px',
+    float: 'right'
+  },
+  hidden: {
+    display: 'none'
+  },
+  container: {
+    position: 'relative',
+    paddingLeft: '0',
+    paddingRight: '0'
+  },
+  checkIcon: {
+    color: 'rgb(80, 161, 79)'
+  }
+}));
 SyntaxHighlighter.registerLanguage("javascript", javascript);
 SyntaxHighlighter.registerLanguage("typescript", typescript);
 SyntaxHighlighter.registerLanguage("json", json);
@@ -27,16 +50,61 @@ SyntaxHighlighter.registerLanguage("shell", shell);
 
 function CodeBlock(props: SyntaxHighlighterProps) {
   const darkMode = useDarkMode();
+  const classes = useStyles(darkMode);
+
+  const [timer, setTimer] = React.useState(0);
+  const [showCopy, setShowCopy] = React.useState(false);
+  const [copied, setCopied] = React.useState(false);
+
+  function onContainerEnter() {
+    clearTimeout(timer);
+    setShowCopy(true);
+  }
+
+  function onContainerLeave() {
+    clearTimeout(timer);
+    let timerId = setTimeout(() => {
+      setShowCopy(false);
+    }, 500);
+    setTimer(timerId)
+  }
+
+  function onCopy() {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1300)
+  }
+
   return (
-    <SyntaxHighlighter
-      style={darkMode ? darkTheme : lightTheme}
-      language={props.language || "js"}
-      showLineNumbers={props.showLineNumbers || false}
-      wrapLines={true}
-      lineProps={props.lineProps}
-    >
-      {props.value}
-    </SyntaxHighlighter>
+    <Container
+      onMouseEnter={onContainerEnter}
+      onMouseLeave={onContainerLeave}
+      className={classes.container}>
+      <Zoom in={showCopy || copied} style={{ transitionDelay: '50ms' }}>
+        <CopyToClipboard
+          text={props.value}
+          onCopy={onCopy}>
+          <IconButton
+            aria-label="copy"
+            size="small"
+            className={`${classes.copyButton}`}>
+            {copied ?
+              <CheckIcon fontSize="inherit" className={classes.checkIcon} /> :
+              <FileCopyIcon fontSize="inherit" /> }
+          </IconButton>
+        </CopyToClipboard>
+      </Zoom>
+      <SyntaxHighlighter
+        style={darkMode ? darkTheme : lightTheme}
+        language={props.language || "js"}
+        showLineNumbers={props.showLineNumbers || false}
+        wrapLines={true}
+        lineProps={props.lineProps}
+      >
+        {props.value}
+      </SyntaxHighlighter>
+    </Container >
   );
 }
 
