6bff71b16adce18282b948db5841544cb57ad235
Ry Dahl <ry@tinyclouds.org>
2020-01-21 12:07:10 -0500

    fix example new line char (#203)

--- src/page/Home.js
+++ src/page/Home.js
@@ -7,7 +7,7 @@ const code = `import { serve } from "https://deno.land/std@v0.30.0/http/server.t
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({ body: "Hello World\\n" });
 }
 `;
 
