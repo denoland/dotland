05bfc5c9a0dd9ff64b37406588afc79fbbc9c43c
Yoshiya Hinosawa <stibium121@gmail.com>
2020-01-22 14:14:01 +0900

    chore: fmt

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 
 type Props = {
-  children: string | string[],
-}
+  children: string | string[];
+};
 
 export default ({ children }: Props) => {
   useEffect(() => {
     const originalTitle = document.title;
-    document.title = Array.isArray(children) ? children.join('') : children;
+    document.title = Array.isArray(children) ? children.join("") : children;
     return () => {
       document.title = originalTitle;
-    }
+    };
   }, [children]);
 
   return null;
