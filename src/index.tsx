49e629b9447c0e038d5e4a0c5da62e95fd7a76be
Luca Casonato <lucacasonato@yahoo.com>
2020-02-20 10:26:28 +0100

    Added tests for trailing slashes

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
 import App from "./App";
 import { generateTheme, useDarkMode } from "./hook/theme";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(<Index />, document.getElementById("root"));
 
@@ -15,7 +16,9 @@ function Index() {
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
