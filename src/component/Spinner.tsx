c94017d32b636061416defd70ba638e58242aa67
dbrands <david.brands@me.com>
2020-04-22 20:41:37 +0200

    Centered spinner

---
+++ src/component/Spinner.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CircularProgress, makeStyles } from "@material-ui/core";
+import { useDarkMode } from "../hook/theme";
+
+const useStyles = makeStyles(_theme => ({
+  spinner: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+}));
+
+export default function Spinner() {
+  const darkMode = useDarkMode();
+  const classes = useStyles(darkMode);
+
+  return (
+    <div className={classes.spinner}>
+      <CircularProgress />
+    </div>
+  );
+}
