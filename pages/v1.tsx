e9753f359247d5410faa8ca52872a1963174510c
Luca Casonato <lucacasonato@yahoo.com>
2020-05-13 01:49:37 +0200

    Added hoodie purchase page and link on v1 page (#37)

--- pages/v1.tsx
+++ pages/v1.tsx
@@ -8,6 +8,7 @@ import Header from "../components/Header";
 import Markdown from "../components/Markdown";
 import { GetStaticProps } from "next";
 import { promises as fs } from "fs";
+import Link from "next/link";
 
 interface Props {
   markdown: string;
@@ -36,16 +37,34 @@ const V1 = (props: Props) => {
           className="max-w-screen-lg mx-auto w-full block md:hidden"
         />
       </div>
-      <div className="max-w-screen-md mx-auto px-4 sm:px-6 md:px-8 py-8">
+      <div className="max-w-screen-md mx-auto px-4 sm:px-6 md:px-8 py-8 mb-16">
         <h1 className="text-3xl tracking-tight font-bold text-5xl leading-10 mt-4">
           Deno 1.0
         </h1>
         <p className="text-gray-500 mt-3 leading-tight">
           May 13th 2020 – Ryan Dahl and Bert Belder
         </p>
-        <p className="text-gray-500 mt-1 leading-tight"></p>
         <div className="mt-8">
           <Markdown source={props.markdown} />
+        </div>
+        <div className="mt-6 py-32">
+          <h2 className="text-3xl tracking-tight font-bold ">One last thing</h2>
+          <p>
+            Help support this work by pre-purchasing a limited time special
+            edition Deno v1.0 hoodie:
+          </p>
+          <img
+            src="/v1_hoodie_mock.png"
+            alt="deno 1.0 hoodie"
+            className="mx-auto p-8 max-w-sm"
+          />
+          <span className="block rounded-md shadow-sm">
+            <Link href="/v1/hoodie">
+              <a className="flex  justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:shadow-outline-indigo active:bg-gray-700 transition duration-150 ease-in-out">
+                Buy it here &rarr;
+              </a>
+            </Link>
+          </span>
         </div>
       </div>
       <Footer />
