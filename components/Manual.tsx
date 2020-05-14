5ae3a97fac937f7c6a99cb5a581353252cfb1ba0
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-05-14 23:20:14 +0530

    Add background color to search input

--- components/Manual.tsx
+++ components/Manual.tsx
@@ -243,7 +243,7 @@ function Manual() {
                     </div>
                     <input
                       id="search_field"
-                      className="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm"
+                      className="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm"
                       placeholder="Search"
                       type="search"
                     />
