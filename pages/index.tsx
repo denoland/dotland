7c5a1740f6c6a705b736b2f8e23790b9f99c1ae0
Lucas Viana <vianadossantoslucas@gmail.com>
2020-05-15 12:57:40 -0300

    :mag: adds support to OpenGraph in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,9 +47,37 @@ const Home: NextPage<HomeProps> = ({ thirdPartyEntries }) => {
       <Head>
         <title>Deno</title>
         <meta
-          name="description"
-          content="Deno, a secure runtime for JavaScript and TypeScript."
+          name="title"
+          content="Deno — A secure runtime for JavaScript and TypeScript."
         />
+        <meta
+          name="description"
+          content="Deno is a simple, modern and secure runtime for JavaScript and TypeScript that uses V8 and is built in Rust."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://deno.land/" />
+        <meta
+          property="og:title"
+          content="Deno — A secure runtime for JavaScript and TypeScript."
+        />
+        <meta
+          property="og:description"
+          content="Deno is a simple, modern and secure runtime for JavaScript and TypeScript that uses V8 and is built in Rust."
+        />
+        <meta property="og:image" content="/v1_wide.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://deno.land/" />
+        <meta
+          property="twitter:title"
+          content="Deno — A secure runtime for JavaScript and TypeScript."
+        />
+        <meta
+          property="twitter:description"
+          content="Deno is a simple, modern and secure runtime for JavaScript and TypeScript that uses V8 and is built in Rust."
+        />
+        <meta property="twitter:image" content="/v1_wide.jpg" />
       </Head>
       <div className="bg-white">
         <div className="bg-black">
