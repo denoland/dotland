5155e5d5f8822611f61fed276d95b9f796bcb1ea
Luuk de Vlieger <info@luuk.site>
2020-05-17 18:08:42 +0200

    Remove some unused things

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,6 @@ const purgecss = [
 ];
 module.exports = {
   plugins: [
-    "postcss-import",
     "tailwindcss",
     "autoprefixer",
     ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
