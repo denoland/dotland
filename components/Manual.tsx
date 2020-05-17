bbcefef1661b2b51af51b6a0999855a150509e00
Kid <44045911+kidonng@users.noreply.github.com>
2020-05-17 23:32:13 +0800

    Small tweaks (#549)
    * 2 spaces for tab

--- components/Manual.tsx
+++ components/Manual.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter, Router } from "next/router";
@@ -46,6 +46,12 @@ function Manual() {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
 
   Router.events.on("routeChangeStart", () => setShowSidebar(false));
+
+  const manualEl = useRef<HTMLElement>(null);
+
+  Router.events.on("routeChangeComplete", () =>
+    manualEl.current?.scrollTo(0, 0)
+  );
 
   useEffect(() => {
     if (showSidebar) {
@@ -297,6 +303,7 @@ function Manual() {
           <main
             className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
             tabIndex={0}
+            ref={manualEl}
           >
             <div className="max-w-screen-md mx-auto px-4 sm:px-6 md:px-8 pb-12 sm:pb-20">
               {content ? (
