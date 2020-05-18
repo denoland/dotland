4668977cfd91b794cbf0587391270de64c64be17
Luca Casonato <luca.casonato@antipy.com>
2020-05-18 19:18:22 +0200

    Revert "Remove some unused dependencies (#588)"
    This reverts commit acf93c4321abf2d6bf023044a687371e276e6d89.

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,5 +25,5 @@ module.exports = {
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "active"],
   },
-  plugins: [require("@tailwindcss/ui")],
+  plugins: [require("@tailwindcss/ui"), require("tailwindcss-hyphens")],
 };
