a66d5d6c7fceca6a5c2d0175affa990ebb26ae6e
Kid <44045911+kidonng@users.noreply.github.com>
2020-05-18 21:56:35 +0800

    Persist line break in code blocks (#526)

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -53,22 +53,33 @@ export const RawCodeBlock = ({
             )}
           {tokens.length > 1 && !disablePrefixes && (
             <code className="pr-2 sm:pr-3">
-              {tokens.map((line, i) => (
-                <div
-                  key={i + "l"}
-                  className="text-gray-400 token-line text-right select-none"
-                >
-                  {i + 1}{" "}
-                </div>
-              ))}
+              {tokens.map(
+                (line, i) =>
+                  !(i === tokens.length - 1 && line.length === 1) && (
+                    <div
+                      key={i + "l"}
+                      className="text-gray-400 token-line text-right select-none"
+                    >
+                      {i + 1}{" "}
+                    </div>
+                  )
+              )}
             </code>
           )}
           <code>
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
+                {line.length === 1
+                  ? i !== tokens.length - 1 && (
+                      <span
+                        {...getTokenProps({ token: line[0], key: 0 })}
+                        // eslint-disable-next-line react/no-children-prop
+                        children={"\n"}
+                      />
+                    )
+                  : line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
               </div>
             ))}
           </code>
