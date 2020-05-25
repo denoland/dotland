fac760d35f5ba4a67f0df68bacce6b0f10d3e374
Kennan Seno <7852866+kennanseno@users.noreply.github.com>
2020-05-25 21:29:13 +0100

    Fix misleading size for directories (#796)

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -519,9 +519,9 @@ function DirectoryListing(props: {
                           {entry.name}
                         </td>
                         <td className="px-4 py-1 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right">
-                          {entry.size !== undefined
+                          {entry.type !== "dir" && entry.size
                             ? bytesToSize(entry.size)
-                            : "N/A"}
+                            : ""}
                         </td>
                       </tr>
                     </Link>
