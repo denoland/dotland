abdc6b88791c088920536a3b2664903e453173fc
Joydip Roy <joydipand@gmail.com>
2020-05-27 17:40:55 +0530

    fix: purge css warning (#801)

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
-    defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-  },
-];
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
