58ffd6b70b75a6e74f5ff1cf6927700e8306e69f
keiya01 <keiya.s.0210@gmail.com>
2020-05-30 21:39:14 +0900

    Fixed Header component style

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,7 +60,7 @@ const Header = ({ subtitle }: { subtitle?: string }) => {
           </Link>
           <a
             href="https://doc.deno.land/https/github.com/denoland/deno/releases/latest/download/lib.deno.d.ts"
-            className="ml-10 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out"
+            className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
           >
             Runtime API
           </a>
