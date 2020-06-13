6091325deda4a9bbc4cc51cdd4a32097426a6d6d
Luca Casonato <lucacasonato@yahoo.com>
2020-06-14 01:41:52 +0200

    Add a dot

--- components/InfoBar.tsx
+++ components/InfoBar.tsx
@@ -5,7 +5,7 @@ export function InfoBar() {
     <div className="bg-black">
       <div className="max-w-screen-xl mx-auto py-8 sm:py-6 px-3 sm:px-6 lg:px-8">
         <p className="font-medium text-xl text-white flex flex-col lg:flex-row justify-start lg:justify-between w-full">
-          <div className="font-bold">Black Lives Matter</div>
+          <div className="font-bold">Black Lives Matter.</div>
           <div>
             <a
               className="text-white underline"
