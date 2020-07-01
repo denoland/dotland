b9279c3ab62827c537a3aa8aa21e1e2c6aa8569d
Luca Casonato <lucacasonato@yahoo.com>
2020-07-02 01:18:22 +0200

    Deprecate gh: and npm: redirects

--- worker/__tests__/handler.ts
+++ worker/__tests__/handler.ts
@@ -1,6 +1,6 @@
 import { fetch, URL, Request, Response } from "@dollarshaveclub/cloudworker";
 import { handleRequest } from "../src/handler";
-import { needsWarning } from "../src/registry";
+import { needsMasterBranchWarning } from "../src/registry";
 
 /* eslint-env jest */
 
@@ -14,14 +14,31 @@ describe("worker proxying", () => {
     const result = await handleRequest(new Request("https://deno.land/"));
     expect(result.headers.get("Content-Type")).toContain("text/html");
     const text = await result.text();
-    expect(text).toContain('Deno');
+    expect(text).toContain("Deno");
   }, 5000);
 
   test("needsWarning", async () => {
-    expect(needsWarning("/std/http/server.ts")).toEqual(true);
-    expect(needsWarning("/std@v0.1.2/http/server.ts")).toEqual(false);
-    expect(needsWarning("/x/foo/bar.txt")).toEqual(false);
-    expect(needsWarning("/index.html")).toEqual(false);
+    expect(
+      needsMasterBranchWarning({
+        name: "std",
+        version: undefined,
+        url: "https://someurl",
+      })
+    ).toEqual(true);
+    expect(
+      needsMasterBranchWarning({
+        name: "std",
+        version: "v0.1.2",
+        url: "https://someurl",
+      })
+    ).toEqual(false);
+    expect(
+      needsMasterBranchWarning({
+        name: "foo",
+        version: undefined,
+        url: "https://someurl",
+      })
+    ).toEqual(false);
   });
 
   it("/typedoc/ responds with typedoc", async () => {
@@ -41,7 +58,7 @@ describe("worker proxying", () => {
     );
     expect(result.headers.get("Content-Type")).toContain("text/html");
     const text = await result.text();
-    expect(text).toContain('Deno');
+    expect(text).toContain("Deno");
   }, 5000);
 
   it("/std/http/server.ts had x-deno-warning", async () => {
@@ -67,7 +84,7 @@ describe("worker proxying", () => {
     );
     expect(result.headers.get("Content-Type")).toContain("text/html");
     const text = await result.text();
-    expect(text).toContain('Deno');
+    expect(text).toContain("Deno");
   }, 5000);
 
   it("/x/std/version.ts with no Accept responds with raw markdown", async () => {
@@ -89,7 +106,7 @@ describe("worker proxying", () => {
     );
     expect(result.headers.get("Content-Type")).toContain("text/html");
     const text = await result.text();
-    expect(text).toContain('Deno');
+    expect(text).toContain("Deno");
   }, 5000);
 
   it("/std@v0.50.0/version.ts with no Accept responds with raw markdown", async () => {
@@ -111,7 +128,7 @@ describe("worker proxying", () => {
     );
     expect(result.headers.get("Content-Type")).toContain("text/html");
     const text = await result.text();
-    expect(text).toContain('Deno');
+    expect(text).toContain("Deno");
   }, 5000);
 
   it("/x/std@v0.50.0/version.ts with no Accept responds with raw markdown", async () => {
