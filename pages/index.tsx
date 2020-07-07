b99fb029b1f56abe5a8d690168b4badf3e26e8a4
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-07-07 15:03:53 +0800

    chore: format

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,9 +125,7 @@ for await (const req of s) {
           />
           <p className="my-4 text-gray-700">Or a more complex one:</p>
         </div>
-        <div
-          className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8"
-        >
+        <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8">
           <CodeBlock
             code={complexExampleProgram}
             language="typescript"
