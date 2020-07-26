0567fafbb02c83e76570db1ad1796a3c3825624e
crowlKats <13135287+crowlKats@users.noreply.github.com>
2020-07-26 22:33:43 +0200

    Remove double artwork link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -53,13 +53,6 @@ const Footer = (props: { simple?: boolean }) => (
           </Link>
         </div>
         <div className="px-5 py-2">
-          <Link href="/artwork">
-            <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              Artwork
-            </a>
-          </Link>
-        </div>
-        <div className="px-5 py-2">
           <Link href="/v1">
             <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
               v1.0 Annoucement
