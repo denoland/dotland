292a55ccf9d38a6b26d0934727293c0aa349447b
Luca Casonato <lucacasonato@yahoo.com>
2020-07-28 13:50:39 +0200

    Remvoe typedoc

--- worker/src/handler.ts
+++ worker/src/handler.ts
@@ -3,8 +3,6 @@
 import { handleRegistryRequest } from "./registry";
 
 const REMOTE_URL = "https://deno-website2.now.sh";
-const TYPEDOC_REMOTE_URL =
-  "http://deno.land.s3-website-us-east-1.amazonaws.com";
 
 export async function handleRequest(request: Request) {
   const accept = request.headers.get("accept");
@@ -12,12 +10,8 @@ export async function handleRequest(request: Request) {
 
   const url = new URL(request.url);
 
-  if (url.pathname.startsWith("/typedoc")) {
-    return proxyFile(url, TYPEDOC_REMOTE_URL, request);
-  }
-
-  const isRegistryRequest =
-    url.pathname.startsWith("/std") || url.pathname.startsWith("/x");
+  const isRegistryRequest = url.pathname.startsWith("/std") ||
+    url.pathname.startsWith("/x");
 
   if (isRegistryRequest && !isHtml) {
     return handleRegistryRequest(url);
