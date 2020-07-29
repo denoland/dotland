43dbfe505aaad86547990d3e11000b9f64c123e2
Luca Casonato <lucacasonato@yahoo.com>
2020-07-29 11:51:51 +0200

    Implement implicit version redirects

--- worker/src/registry.ts
+++ worker/src/registry.ts
@@ -1,27 +1,50 @@
 import { parseNameVersion } from "../../util/registry_utils";
+import { version } from "punycode";
 
 const S3_BUCKET =
   "http://deno-registry-prod-storagebucket-d7uq3yal946u.s3-website-us-east-1.amazonaws.com/";
 
 export async function handleRegistryRequest(url: URL): Promise<Response> {
   console.log("registry request", url.pathname);
-  const remoteUrl = getRegistrySourceURL(url.pathname);
-  if (!remoteUrl) {
-    return new Response("Module does not exist: " + url.pathname, {
-      status: 404,
-      statusText: "Not Found",
+  const entry = parsePathname(url.pathname);
+  if (!entry) {
+    return new Response("This module entry is invalid: " + url.pathname, {
+      status: 400,
       headers: { "content-type": "text/plain" },
     });
   }
+  const { module, version, path } = entry;
+  if (!version) {
+    const latest = await getLatestVersion(module);
+    if (!latest) {
+      return new Response(
+        "This module has no latest version: " + url.pathname,
+        {
+          status: 404,
+          headers: { "content-type": "text/plain" },
+        },
+      );
+    }
+    console.log("registry redirect", module, latest);
+    return Response.redirect(
+      (`${url.origin}${
+        module === "std" ? "" : "/x"
+      }/${module}@${latest}/${path}`),
+      302,
+    );
+  }
+  const remoteUrl = getBackingURL(module, version, path);
   const resp = await fetch(remoteUrl, { cf: { cacheEverything: true } });
   const resp2 = new Response(resp.body, resp);
   resp2.headers.set("Access-Control-Allow-Origin", "*");
   return resp2;
 }
 
-export function getRegistrySourceURL(pathname: string): string | undefined {
+export function parsePathname(
+  pathname: string,
+): { module: string; version: string | undefined; path: string } | undefined {
   if (pathname.startsWith("/std")) {
-    return getRegistrySourceURL("/x" + pathname);
+    return parsePathname("/x" + pathname);
   }
   if (!pathname.startsWith("/x/")) {
     return undefined;
@@ -29,7 +52,19 @@ export function getRegistrySourceURL(pathname: string): string | undefined {
   const nameBranchRest = pathname.replace(/^\/x\//, "");
   const [nameBranch, ...rest] = nameBranchRest.split("/");
   const [name, version] = parseNameVersion(nameBranch);
-  if (!version) return undefined;
   const path = rest.join("/");
-  return `${S3_BUCKET}${name}/versions/${version}/raw/${path}`;
+  return { module: name, version, path };
+}
+
+export function getBackingURL(module: string, version: string, path: string) {
+  return `${S3_BUCKET}${module}/versions/${version}/raw/${path}`;
+}
+
+export async function getLatestVersion(
+  module: string,
+): Promise<string | undefined> {
+  const res = await fetch(`${S3_BUCKET}${module}/meta/versions.json`);
+  if (!res.ok) return undefined;
+  const versions = await res.json();
+  return versions?.latest;
 }
