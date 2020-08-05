973edbf07397140f245112b554a81baae026ed93
迷渡 <justjavac@gmail.com>
2020-08-05 23:29:52 +0800

    fix: url of repo source (#1426)
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- pages/status/[id].tsx
+++ pages/status/[id].tsx
@@ -78,7 +78,9 @@ const RestPage = () => {
                   </dt>
                   <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                     <a
-                      href={`https://github.com/${data.options.repository}/blob/${data.options.ref}/${data.options.subdir}`}
+                      href={`https://github.com/${
+                        data.options.repository
+                      }/blob/${data.options.ref}/${data.options.subdir ?? ""}`}
                       className="link"
                     >
                       View on GitHub
