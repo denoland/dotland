d8b2edf646edc8624ded9766afa28a851b1f8fcf
Luca Casonato <lucacasonato@yahoo.com>
2020-08-19 11:59:12 +0200

    Add pagination utils tests

---
+++ util/pagination_utils.test.ts
@@ -0,0 +1,28 @@
+/* eslint-env jest */
+
+import { hasPrevious, hasNext, pageCount } from "./pagination_utils";
+
+test("hasPrevious", () => {
+  expect(hasPrevious({ page: 0 })).toEqual(false);
+  expect(hasPrevious({ page: 1 })).toEqual(false);
+  expect(hasPrevious({ page: 2 })).toEqual(true);
+  expect(hasPrevious({ page: 5 })).toEqual(true);
+});
+
+test("hasNext", () => {
+  expect(hasNext({ page: 1, totalCount: 30, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 5, totalCount: 30, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 6, totalCount: 30, perPage: 5 })).toEqual(false);
+  expect(hasNext({ page: 1, totalCount: 29, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 5, totalCount: 29, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 6, totalCount: 29, perPage: 5 })).toEqual(false);
+  expect(hasNext({ page: 1, totalCount: 31, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 5, totalCount: 31, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 6, totalCount: 31, perPage: 5 })).toEqual(true);
+  expect(hasNext({ page: 7, totalCount: 31, perPage: 5 })).toEqual(false);
+});
+
+test("pageCount", () => {
+  expect(pageCount({ totalCount: 30, perPage: 5 })).toEqual(6);
+  expect(pageCount({ totalCount: 31, perPage: 5 })).toEqual(7);
+});
