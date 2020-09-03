c62b4d3b173591b4e90b3a61facac14810739589
Galileo <32818066+Galileo-dev@users.noreply.github.com>
2020-09-03 14:32:21 +0100

    fix: Scroll element into view on hash change (#1496)

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -52,10 +52,13 @@ export const RawCodeBlock = ({
         : target;
     }
   };
+
   if (enableLineRef) {
     useEffect(() => {
       const onHashChange = () => {
         setHashValue(location.hash);
+        const id = location.hash.substring(1);
+        document.getElementById(id)?.scrollIntoView();
       };
       window.addEventListener("hashchange", onHashChange);
       onHashChange();
