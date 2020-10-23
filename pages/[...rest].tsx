8259485c7d415d7dd6388c9b0c036d66d7da6c64
Dezső Mészáros <meszarosdezsodev@gmail.com>
2020-10-23 22:57:05 +0200

    Custom 404 page 🦕 (#1570)

--- pages/[...rest].tsx
+++ pages/[...rest].tsx
@@ -2,11 +2,11 @@
 
 import React, { useMemo } from "react";
 import { useRouter } from "next/router";
-import ErrorPage from "next/error";
 import Registry from "../components/Registry";
 import { parseNameVersion } from "../util/registry_utils";
 import Manual from "../components/Manual";
 import LoadingPage from "../components/LoadingPage";
+import NotFoundPage from "../components/NotFound";
 
 function RestPage(): React.ReactElement {
   const { query } = useRouter();
@@ -21,7 +21,7 @@ function RestPage(): React.ReactElement {
 
   if (name === "") return <LoadingPage />;
 
-  return <ErrorPage statusCode={404} />;
+  return <NotFoundPage />;
 }
 
 export default RestPage;
