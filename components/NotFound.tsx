ba1d67f5cf2c8ab4833e38118949d2bae8eb562d
Dezső Mészáros <meszarosdezsodev@gmail.com>
2020-10-24 17:01:28 +0200

    [Fix] [404] Fixed unneccessary scrolling (#1578)

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -5,23 +5,25 @@ import Header from "./Header";
 function NotFoundPage(): React.ReactElement {
   return (
     <div className="NotFoundPage">
-      <Header />
-      <header>
-        <h1 className="font-extrabold text-5xl leading-10 tracking-tight text-gray-900">
-          404
-        </h1>
-        <h2 className="mt-4 sm:mt-5 font-light text-2xl text-center leading-tight text-gray-900">
-          Couldn't find what you're looking for.
-        </h2>
-      </header>
+      <div id="flex-top">
+        <Header />
+        <header>
+          <h1 className="font-extrabold text-5xl leading-10 tracking-tight text-gray-900">
+            404
+          </h1>
+          <h2 className="mt-4 sm:mt-5 font-light text-2xl text-center leading-tight text-gray-900">
+            Couldn't find what you're looking for.
+          </h2>
+        </header>
+      </div>
 
-      <div className="animation">
-        <img src="images/ferris.gif" alt="Ferris" id="ferris404" />
-        <img src="images/deno404.gif" alt="Deno not found ):" id="deno404" />
-
-        <div className="land bg-gray-300">
-          <Footer />
+      <div id="flex-bottom">
+        <div id="animation">
+          <img src="images/ferris.gif" alt="Ferris" id="ferris404" />
+          <img src="images/deno404.gif" alt="Deno" id="deno404" />
         </div>
+
+        <Footer />
       </div>
     </div>
   );
