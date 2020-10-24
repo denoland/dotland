4c44ffdc64d83410b0e7f36d4e7997ce3efe8ad1
Szalay Kristóf <32012862+littletof@users.noreply.github.com>
2020-10-24 15:43:35 +0200

    feat: add version timestamp as a separate card (#1577)

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -63,6 +63,18 @@ function Registry(): React.ReactElement {
     }
   }
 
+  function getFormattedDateTime(date: Date) {
+    const format = new Intl.DateTimeFormat(undefined, {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      month: "numeric",
+      day: "numeric",
+      year: "numeric",
+    });
+    return format.format(date);
+  }
+
   // Base paths
   const basePath = useMemo(
     () => `${isStd ? "" : "/x"}/${name}${version ? `@${version}` : ""}`,
@@ -490,6 +502,37 @@ function Registry(): React.ReactElement {
                           />
                         </div>
                       </div>
+                    </div>
+
+                    <div className="max-w-sm w-full shadow-sm rounded-lg border border-gray-200 p-4">
+                      <p className="text-md font-semibold mb-2">Version Info</p>
+                      {versionMeta === undefined ? (
+                        <div className="mt-2 flex items-center py-0.5">
+                          <svg
+                            className="h-5 w-5 mr-2 inline text-gray-200"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <title>Tagged at</title>
+                            <path d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                          </svg>
+                          <div className="w-4/5 sm:w-2/3 bg-gray-100 h-4"></div>
+                        </div>
+                      ) : versionMeta === null ? null : (
+                        <div className="mt-2 flex items-center">
+                          <svg
+                            className="h-5 w-5 mr-2 inline text-gray-700"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <title>Tagged at</title>
+                            <path d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                          </svg>
+                          <div>
+                            {getFormattedDateTime(versionMeta.uploadedAt)}
+                          </div>
+                        </div>
+                      )}
                     </div>
                     {documentationURL && externalDependencies !== null ? (
                       <div className="max-w-sm w-full shadow-sm rounded-lg border border-gray-200 p-4">
