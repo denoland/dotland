531760f3f76557877d561968ad230ee2fce6d74f
Luca Casonato <lucacasonato@yahoo.com>
2020-12-10 23:08:03 +0100

    add cf analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,11 @@ export default class DenoDocDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "1b59386cd9134d5e81c9b0d5b9cb9686"}'
+          ></script>
         </body>
       </Html>
     );
