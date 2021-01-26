dfa1a2a704ae941f6e61dadf28d7c9e2c8ca4189
迷渡 <justjavac@gmail.com>
2021-01-26 09:50:37 +0800

    翻译 post
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -34,13 +34,13 @@ function PostsIndexPage(props: Props): React.ReactElement | null {
   return (
     <>
       <Head>
-        <title>News | Deno</title>
+        <title>新闻 | Deno</title>
       </Head>
       <Header />
       <div className="bg-white pt-8 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8">
         <div className="relative max-w-screen-lg mx-auto">
           <div className="border-b-2 border-gray-100 pb-10">
-            <h2 className="text-4xl font-bold tracking-tight">News</h2>
+            <h2 className="text-4xl font-bold tracking-tight">新闻</h2>
           </div>
           <div className="mt-6 grid gap-16 lg:grid-cols-2 lg:col-gap-5 lg:row-gap-12">
             {props.posts
@@ -76,7 +76,7 @@ function PostsIndexPage(props: Props): React.ReactElement | null {
                     <div className="mt-3">
                       <Link href={`/posts/${post.id}`}>
                         <a className="text-base leading-6 font-semibold text-blue-600 hover:text-blue-500 transition ease-in-out duration-150">
-                          Read post
+                          阅读原文
                         </a>
                       </Link>
                     </div>
