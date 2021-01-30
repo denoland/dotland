31cc380463888cf4ae58c92e05efcb163840301b
Daniel Mizerski <duelsik@gmail.com>
2021-01-30 11:21:06 +0100

    fix(notfound page): serve with 404 code

--- pages/[...rest].tsx
+++ pages/[...rest].tsx
@@ -1,6 +1,7 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
 import React, { useMemo } from "react";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import Registry from "../components/Registry";
 import { parseNameVersion } from "../util/registry_utils";
@@ -24,4 +25,17 @@ function RestPage(): React.ReactElement {
   return <NotFoundPage />;
 }
 
+const restPagePaths = ["/std", "/manual"];
+const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const url = req.url ?? "";
+  const isUrlFound = restPagePaths.some((whitelistedPath) =>
+    url.startsWith(whitelistedPath)
+  );
+  if (!isUrlFound) {
+    res.statusCode = 404;
+  }
+  return { props: {} };
+};
+
+export { getServerSideProps };
 export default RestPage;
