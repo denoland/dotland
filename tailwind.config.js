629d76c9577318b149bb4258acbbd7ba05b2c7be
Jheyson Saavedra <JheysonDev@outlook.com>
2021-02-25 10:06:25 -0500

    feat: add button for toggle theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ const defaultTheme = require("tailwindcss/defaultTheme"); // eslint-disable-line
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  darkMode: "media",
+  darkMode: "class",
   theme: {
     colors: {
       transparent: "transparent",
       current: "currentColor",
       white: colors.white,
+      black: colors.black,
       gray: colors.gray,
       red: colors.red,
       blue: colors.blue,
       yellow: colors.yellow,
-      "black-950": colors.black,
       "black-900": "#141414",
       "black-800": "#1a1a1a",
       "black-700": "#202020",
