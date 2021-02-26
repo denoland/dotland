4f20e990753fd98935cda3f6793a37c9af4dc3d7
Jheyson Saavedra <JheysonDev@outlook.com>
2021-02-26 17:31:19 -0500

    resolve merge

--- worker/src/index.ts
+++
@@ -1,7 +0,0 @@
-/* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
-
-import { handleRequest } from "./handler";
-
-addEventListener("fetch", (event: FetchEvent) => {
-  event.respondWith(handleRequest(event.request));
-});
