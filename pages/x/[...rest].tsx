5c031aba9abbb5f606489c8eb446a6ab961a4202
Jheyson Saavedra <JheysonDev@outlook.com>
2021-02-26 19:03:29 -0500

    fix: Force redirect from /x/std to /std

--- pages/x/[...rest].tsx
+++ pages/x/[...rest].tsx
@@ -1,10 +1,15 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-import React from "react";
-
+import React, { useEffect } from "react";
+import {useRouter} from "next/router"
 import Registry from "../../components/Registry";
 
 function RegistryPage(): React.ReactElement {
+  const router = useRouter();
+  if (router.asPath.startsWith("/x/std")) {
+    router.replace("/std");
+  }
+
   return <Registry />;
 }
 
