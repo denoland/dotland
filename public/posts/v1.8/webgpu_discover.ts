abe0aa4812ad0092134aa6ac68994b262a6710ff
Luca Casonato <lucacasonato@yahoo.com>
2021-03-02 15:13:48 +0100

    edits and 1.8 release header

--- public/posts/v1.8/webgpu_discover.ts
+++ public/posts/v1.8/webgpu_discover.ts
@@ -1,13 +1,12 @@
 // Run with `deno run https://deno.land/posts/v1.8/webgpu_discover.ts`
 
-// Try to get an adapter from the user agent. If this fails, return an error.
+// Try to get an adapter from the user agent.
 const adapter = await navigator.gpu.requestAdapter();
-if (!adapter) {
+if (adapter) {
+  // Print out some basic details about the adapter.
+  console.log(`Found adapter: ${adapter.name}`);
+  const features = [...adapter.features.values()];
+  console.log(`Supported features: ${features.join(", ")}`);
+} else {
   console.error("No adapter found");
-  Deno.exit(1);
 }
-
-// Print out some basic details about the adapter.
-console.log(`Found adapter: ${adapter.name}`);
-const features = [...adapter.features.values()];
-console.log(`Supported features: ${features.join(", ")}`);
