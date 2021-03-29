904709688db45c812f7c9889bb66e7e44ab33c9a
Yoshiya Hinosawa <stibium121@gmail.com>
2021-03-29 20:22:56 +0900

    redirect to new blog (#1720)
    Co-authored-by: Luca Casonato <lucacasonato@yahoo.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -56,11 +56,12 @@ function Footer(props: { simple?: boolean }): React.ReactElement {
             </Link>
           </div>
           <div className="px-5 py-2">
-            <Link href="/posts">
-              <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                News
-              </a>
-            </Link>
+            <a
+              href="https://deno.com/blog"
+              className="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              Blog
+            </a>
           </div>
           <div className="px-5 py-2">
             <Link href="/translations">
