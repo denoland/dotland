d4fc5d004b22b82c095e4c7ae52453f3bbd851ef
Luca Casonato <lucacasonato@yahoo.com>
2021-03-29 12:31:25 +0200

    move redirects out of experimental

--- next.config.js
+++ next.config.js
@@ -4,29 +4,29 @@ module.exports = withPreact({
   experimental: {
     modern: true,
     polyfillsOptimization: true,
-    redirects() {
-      return [
-        {
-          source: "/manual.html",
-          destination: "/manual",
-          permanent: true,
-        },
-        {
-          source: "/benchmarks.html",
-          destination: "/benchmarks",
-          permanent: true,
-        },
-        {
-          source: "/posts",
-          destination: "https://deno.com/blog",
-          permanent: true,
-        },
-        {
-          source: "/posts/:id",
-          destination: "https://deno.com/blog/:id",
-          permanent: true,
-        },
-      ];
-    },
+  },
+  redirects() {
+    return [
+      {
+        source: "/manual.html",
+        destination: "/manual",
+        permanent: true,
+      },
+      {
+        source: "/benchmarks.html",
+        destination: "/benchmarks",
+        permanent: true,
+      },
+      {
+        source: "/posts",
+        destination: "https://deno.com/blog",
+        permanent: true,
+      },
+      {
+        source: "/posts/:id",
+        destination: "https://deno.com/blog/:id",
+        permanent: true,
+      },
+    ];
   },
 });
