59f31ca0b4d4f995a17bcf1c772d3e0f0061c109
迷渡 <justjavac@gmail.com>
2021-04-19 10:33:39 +0800

    解决冲突
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,17 +31,10 @@ for await (const req of s) {
       <CookieBanner />
       <div className="bg-blue-500 p-4 text-white flex justify-center text-center">
         <div className="max-w-screen-xl">
-<<<<<<< HEAD
-          <span className="inline">介绍 Deno 公司</span>
-          <span className="block sm:ml-2 sm:inline-block font-semibold">
-            <a href="https://deno.js.cn/t/topic/735">
-              阅读文章 <span aria-hidden="true">&rarr;</span>
-=======
-          <span className="inline">Deno 1.9 is out.</span>
+          <span className="inline">Deno 1.9 发布了</span>
           <span className="block sm:ml-2 sm:inline-block font-semibold">
             <a href="https://deno.com/blog/v1.9">
-              Read the release notes <span aria-hidden="true">&rarr;</span>
->>>>>>> 4a7f4e4ddcbe4faeb771d485c3e779f34bf1c2e5
+              阅读文章 <span aria-hidden="true">&rarr;</span>
             </a>
           </span>
         </div>
