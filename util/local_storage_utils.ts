a859b734ca32398efc761e4e39f55aea03d65eb3
Yoshiya Hinosawa <stibium121@gmail.com>
2021-07-07 18:47:40 +0900

    fix: fix lint warnings

--- util/local_storage_utils.ts
+++ util/local_storage_utils.ts
@@ -1,6 +1,6 @@
 // Copyright 2020-2021 the Deno authors. All rights reserved. MIT license.
 
-export function getItem(key: string) {
+export function getItem(key: string): string | null {
   try {
     return window.localStorage.getItem(key);
   } catch (_e) {
@@ -8,7 +8,7 @@ export function getItem(key: string) {
   }
 }
 
-export function setItem(key: string, value: string) {
+export function setItem(key: string, value: string): void {
   try {
     window.localStorage.setItem(key, value);
   } catch (_e) {
