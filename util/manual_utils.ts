37570312072b8c286681fc2dd4908ebed4dfe545
Luca Casonato <hello@lcas.dev>
2021-07-21 00:55:10 +0200

    user contribution warning banner

--- util/manual_utils.ts
+++ util/manual_utils.ts
@@ -35,7 +35,7 @@ function isOldVersion(version: string) {
 }
 
 function basepath(version: string) {
-  if (VERSIONS.cli.find((v) => v === version) === undefined) {
+  if (isPreviewVersion(version)) {
     return githubBasepath + version;
   }
   if (isOldVersion(version)) {
@@ -45,13 +45,14 @@ function basepath(version: string) {
 }
 
 export async function getTableOfContents(
-  version: string,
+  version: string
 ): Promise<TableOfContents> {
   const res = await fetch(`${basepath(version)}/toc.json`);
   if (res.status !== 200) {
     throw Error(
-      `Got an error (${res.status}) while getting the manual table of contents:\n${await res
-        .text()}`,
+      `Got an error (${
+        res.status
+      }) while getting the manual table of contents:\n${await res.text()}`
     );
   }
   return await res.json();
@@ -63,4 +64,8 @@ export function getFileURL(version: string, path: string): string {
 
 export function getDocURL(version: string, path: string): string {
   return `${docpath}${version}/docs${path}.md`;
+}
+
+export function isPreviewVersion(version: string): boolean {
+  return VERSIONS.cli.find((v) => v === version) === undefined;
 }
