725c77384c94fcdbf6187e2645ded8d2577dd53d
Luca Casonato <hello@lcas.dev>
2021-07-21 01:13:27 +0200

    chore: update to use new manual repo (#1801)

--- util/manual_utils.test.ts
+++ util/manual_utils.test.ts
@@ -1,20 +1,32 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-import { getTableOfContents, getFileURL } from "./manual_utils";
+import { getFileURL, getTableOfContents } from "./manual_utils";
 import "isomorphic-unfetch";
 
 /* eslint-env jest */
 
 test("get table of contents", async () => {
   expect(
-    await getTableOfContents("f184332c09c851faac50f598d29ebe4426e05464")
+    await getTableOfContents("95b75e204ab3c0966e344a52c7bc9b9011ac345f")
   ).toBeTruthy();
 });
 
-test("get introduction file", async () => {
+test("get introduction file commit hash", async () => {
   expect(
-    getFileURL("f184332c09c851faac50f598d29ebe4426e05464", "/introduction")
+    getFileURL("95b75e204ab3c0966e344a52c7bc9b9011ac345f", "/introduction")
   ).toEqual(
-    "https://raw.githubusercontent.com/denoland/deno/f184332c09c851faac50f598d29ebe4426e05464/docs/introduction.md"
+    "https://raw.githubusercontent.com/denoland/manual/95b75e204ab3c0966e344a52c7bc9b9011ac345f/introduction.md"
+  );
+});
+
+test("get introduction file old repo", async () => {
+  expect(getFileURL("v1.12.0", "/introduction")).toEqual(
+    "https://deno.land/x/deno@v1.12.0/docs/introduction.md"
+  );
+});
+
+test("get introduction file new repo", async () => {
+  expect(getFileURL("v1.12.1", "/introduction")).toEqual(
+    "https://deno.land/x/manual@v1.12.1/introduction.md"
   );
 });
--- util/manual_utils.ts
+++ util/manual_utils.ts
@@ -1,8 +1,9 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-const xBasepath = "https://deno.land/x/deno@";
-const githubBasepath = "https://raw.githubusercontent.com/denoland/deno/";
-const docpath = "https://github.com/denoland/deno/blob/";
+const oldXBasepath = "https://deno.land/x/deno@";
+const xBasepath = "https://deno.land/x/manual@";
+const githubBasepath = "https://raw.githubusercontent.com/denoland/manual/";
+const docpath = "https://github.com/denoland/manual/blob/";
 import VERSIONS from "../versions.json";
 
 export const versions = VERSIONS.cli;
@@ -16,16 +17,35 @@ export interface TableOfContents {
   };
 }
 
+const SEMVER_REGEX = /v?(\d+)\.(\d+)\.(\d+)/;
+
+// Returns true if the version is of the 0.x release line, or betwen 1.0.0 and
+// 1.12.0 inclusive. During this time the manual was part of the main repo. It
+// is now a seperate repo.
+function isOldVersion(version: string) {
+  const matches = version.match(SEMVER_REGEX);
+  if (!matches) throw new TypeError("This shouldn't have happened!");
+  return (
+    matches[1] === "0" ||
+    (matches[1] === "1" && parseInt(matches[2]) < 12) ||
+    (matches[1] === "1" && matches[2] === "12" && matches[3] === "0")
+  );
+}
+
 function basepath(version: string) {
-  return VERSIONS.cli.find((v) => v === version) === undefined
-    ? githubBasepath
-    : xBasepath;
+  if (isPreviewVersion(version)) {
+    return githubBasepath + version;
+  }
+  if (isOldVersion(version)) {
+    return oldXBasepath + version + "/docs";
+  }
+  return xBasepath + version;
 }
 
 export async function getTableOfContents(
   version: string
 ): Promise<TableOfContents> {
-  const res = await fetch(`${basepath(version)}${version}/docs/toc.json`);
+  const res = await fetch(`${basepath(version)}/toc.json`);
   if (res.status !== 200) {
     throw Error(
       `Got an error (${
@@ -37,9 +57,13 @@ export async function getTableOfContents(
 }
 
 export function getFileURL(version: string, path: string): string {
-  return `${basepath(version)}${version}/docs${path}.md`;
+  return `${basepath(version)}${path}.md`;
 }
 
 export function getDocURL(version: string, path: string): string {
   return `${docpath}${version}/docs${path}.md`;
+}
+
+export function isPreviewVersion(version: string): boolean {
+  return VERSIONS.cli.find((v) => v === version) === undefined;
 }
