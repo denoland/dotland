534885537b839b9cd102f8cbccfe0cf040100f24
Luca Casonato <hello@lcas.dev>
2021-07-20 12:28:15 +0200

    chore: update to use new manual repo

--- util/manual_utils.ts
+++ util/manual_utils.ts
@@ -1,8 +1,9 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-const xBasepath = "https://deno.land/x/deno@";
-const githubBasepath = "https://raw.githubusercontent.com/denoland/deno/";
-const docpath = "https://github.com/denoland/deno/blob/";
+const oldXBasepath = "https://deno.land/x/deno@";
+const xBasepath = "https://deno.land/x/manual@";
+const githubBasepath = "https://raw.githubusercontent.com/denoland/manual/";
+const docpath = "https://github.com/denoland/manual/blob/";
 import VERSIONS from "../versions.json";
 
 export const versions = VERSIONS.cli;
@@ -16,28 +17,44 @@ export interface TableOfContents {
   };
 }
 
+const SEMVER_REGEX = /v?(\d+)\.(\d+)\.(\d+)/;
+
+// Returns true if the version is of the 0.x release line, or betwen 1.0.0 and
+// 1.12.0 inclusive. During this time the manual was part of the main repo. It
+// is now a seperate repo.
+function isOldVersion(version: string) {
+  const matches = version.match(SEMVER_REGEX);
+  if (!matches) throw new TypeError("This shouldn't have happened!");
+  return matches[0] === "0" ||
+    (matches[0] === "1" && parseInt(matches[1]) < 12) ||
+    (matches[0] === "1" || matches[1] === "12" || matches[2] === "0");
+}
+
 function basepath(version: string) {
-  return VERSIONS.cli.find((v) => v === version) === undefined
-    ? githubBasepath
-    : xBasepath;
+  if (VERSIONS.cli.find((v) => v === version) === undefined) {
+    return githubBasepath + version;
+  }
+  if (isOldVersion(version)) {
+    return oldXBasepath + version + "/docs";
+  }
+  return xBasepath + version;
 }
 
 export async function getTableOfContents(
-  version: string
+  version: string,
 ): Promise<TableOfContents> {
-  const res = await fetch(`${basepath(version)}${version}/docs/toc.json`);
+  const res = await fetch(`${basepath(version)}/toc.json`);
   if (res.status !== 200) {
     throw Error(
-      `Got an error (${
-        res.status
-      }) while getting the manual table of contents:\n${await res.text()}`
+      `Got an error (${res.status}) while getting the manual table of contents:\n${await res
+        .text()}`,
     );
   }
   return await res.json();
 }
 
 export function getFileURL(version: string, path: string): string {
-  return `${basepath(version)}${version}/docs${path}.md`;
+  return `${basepath(version)}${path}.md`;
 }
 
 export function getDocURL(version: string, path: string): string {
