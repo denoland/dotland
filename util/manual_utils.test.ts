f407afc59179ab06894dca3578a9bb4e7ee5e424
迷渡 <justjavac@gmail.com>
2021-07-22 08:45:34 +0800

    resolve merging all conflicts（@ 725c773）
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- util/manual_utils.test.ts
+++ util/manual_utils.test.ts
@@ -11,31 +11,11 @@ test("get table of contents", async () => {
   ).toBeTruthy();
 });
 
-<<<<<<< HEAD
 /** @todo 目前中文版只有最新文档 */
 test("get introduction file", async () => {
-=======
-test("get introduction file commit hash", async () => {
->>>>>>> 725c77384c94fcdbf6187e2645ded8d2577dd53d
   expect(
     getFileURL("95b75e204ab3c0966e344a52c7bc9b9011ac345f", "/introduction")
   ).toEqual(
-<<<<<<< HEAD
     "https://cdn.jsdelivr.net/gh/denocn/deno_docs@master/introduction.md"
-=======
-    "https://raw.githubusercontent.com/denoland/manual/95b75e204ab3c0966e344a52c7bc9b9011ac345f/introduction.md"
-  );
-});
-
-test("get introduction file old repo", async () => {
-  expect(getFileURL("v1.12.0", "/introduction")).toEqual(
-    "https://deno.land/x/deno@v1.12.0/docs/introduction.md"
-  );
-});
-
-test("get introduction file new repo", async () => {
-  expect(getFileURL("v1.12.1", "/introduction")).toEqual(
-    "https://deno.land/x/manual@v1.12.1/introduction.md"
->>>>>>> 725c77384c94fcdbf6187e2645ded8d2577dd53d
   );
 });
--- util/manual_utils.ts
+++ util/manual_utils.ts
@@ -1,14 +1,7 @@
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-<<<<<<< HEAD
 const githubBasepath = "https://cdn.jsdelivr.net/gh/denocn/deno_docs@";
 const docpath = "https://github.com/denocn/deno_docs/blob/";
-=======
-const oldXBasepath = "https://deno.land/x/deno@";
-const xBasepath = "https://deno.land/x/manual@";
-const githubBasepath = "https://raw.githubusercontent.com/denoland/manual/";
-const docpath = "https://github.com/denoland/manual/blob/";
->>>>>>> 725c77384c94fcdbf6187e2645ded8d2577dd53d
 import VERSIONS from "../versions.json";
 
 export const versions = VERSIONS.cli;
@@ -22,44 +15,12 @@ export interface TableOfContents {
   };
 }
 
-<<<<<<< HEAD
 export async function getTableOfContents(
   version: string
 ): Promise<TableOfContents> {
   console.log("目前中文文档只有最新版");
   version = "master";
   const res = await fetch(`${githubBasepath}${version}/toc.json`);
-=======
-const SEMVER_REGEX = /v?(\d+)\.(\d+)\.(\d+)/;
-
-// Returns true if the version is of the 0.x release line, or betwen 1.0.0 and
-// 1.12.0 inclusive. During this time the manual was part of the main repo. It
-// is now a seperate repo.
-function isOldVersion(version: string) {
-  const matches = version.match(SEMVER_REGEX);
-  if (!matches) throw new TypeError("This shouldn't have happened!");
-  return (
-    matches[1] === "0" ||
-    (matches[1] === "1" && parseInt(matches[2]) < 12) ||
-    (matches[1] === "1" && matches[2] === "12" && matches[3] === "0")
-  );
-}
-
-function basepath(version: string) {
-  if (isPreviewVersion(version)) {
-    return githubBasepath + version;
-  }
-  if (isOldVersion(version)) {
-    return oldXBasepath + version + "/docs";
-  }
-  return xBasepath + version;
-}
-
-export async function getTableOfContents(
-  version: string
-): Promise<TableOfContents> {
-  const res = await fetch(`${basepath(version)}/toc.json`);
->>>>>>> 725c77384c94fcdbf6187e2645ded8d2577dd53d
   if (res.status !== 200) {
     throw Error(
       `Got an error (${
@@ -71,12 +32,8 @@ export async function getTableOfContents(
 }
 
 export function getFileURL(version: string, path: string): string {
-<<<<<<< HEAD
   version = "master";
   return `${githubBasepath}${version}${path}.md`;
-=======
-  return `${basepath(version)}${path}.md`;
->>>>>>> 725c77384c94fcdbf6187e2645ded8d2577dd53d
 }
 
 export function getDocURL(version: string, path: string): string {
