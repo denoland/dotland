8b059848a92e84ca61b36a252660a0f4c370b646
Nick Hayden <nickhayden-web@outlook.com>
2021-08-11 22:05:59 +1000

    #1776 [FIX] Reformat code to match existing list items

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,8 +85,9 @@ for await (const conn of listener) {
             </li>
             <li>
               Has a number of{" "}
-              <a href="https://github.com/denoland/deno/wiki#companies-interested-in-deno" className="link">companies
-                interested in using and exploring Deno</a>
+              <a href="https://github.com/denoland/deno/wiki#companies-interested-in-deno" className="link">
+                companies interested in using and exploring Deno
+              </a>
             </li>
           </ol>
         </div>
