4b03049314798259c41656d7764f59abd6216f6f
Ryan Dahl <ry@tinyclouds.org>
2021-09-06 09:15:27 -0400

    adjust example

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,16 @@ import { CookieBanner } from "../components/CookieBanner";
 const Home: NextPage = () => {
   const complexExampleProgram = `const listener = Deno.listen({ port: 8000 });
 console.log("http://localhost:8000/");
+
+async function handle(conn) {
+  const requests = Deno.serveHttp(conn);
+  for await (const { respondWith } of requests) {
+    respondWith(new Response("Hello world"));
+  }
+}
+
 for await (const conn of listener) {
-  (async () => {
-    const requests = Deno.serveHttp(conn);
-    for await (const { respondWith } of requests) {
-      respondWith(new Response("Hello world"));
-    }
-  })();
+  handle(conn);
 }`;
 
   return (
