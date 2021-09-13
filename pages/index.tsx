a8ab32cfc84fd0df3d535197f19869332177894c
Satya Rohith <me@satyarohith.com>
2021-09-13 23:27:23 +0530

    fix: remove new line

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ import { CookieBanner } from "../components/CookieBanner";
 
 const Home: NextPage = () => {
   const complexExampleProgram = `import { serve } from "https://deno.land/std@0.106.0/http/server.ts";
-const body = "Hello World\n";
+const body = "Hello World";
 const server = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of server) {
