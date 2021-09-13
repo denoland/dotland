52cf6271e0d43277fd1040c4629764be4ef6f00e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-13 23:20:57 +0800

    Delete worker directory

--- worker/main.ts
+++
@@ -1,9 +0,0 @@
-/* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
-
-import { handleRequest, withLog } from "./handler.ts";
-
-const handler = withLog(handleRequest);
-
-addEventListener("fetch", (event: FetchEvent) => {
-  event.respondWith(handler(event.request));
-});
