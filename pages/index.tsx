a00400e68c4c82973aeb6d39b7d5fa4c3e83785b
Ryan Dahl <ry@tinyclouds.org>
2021-09-14 00:09:49 -0400

    refactor: simplify http example (#1876)

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,10 @@ import { CookieBanner } from "../components/CookieBanner";
 
 const Home: NextPage = () => {
   const complexExampleProgram = `import { serve } from "https://deno.land/std@0.106.0/http/server.ts";
-const body = "Hello World";
 const server = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of server) {
-  req.respond({ body });
+  req.respond({ body: "Hello World" });
 }`;
 
   return (
