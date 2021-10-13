01e8988d617d3fafc8e22bc597c423a746e75c09
Satya Rohith <me@satyarohith.com>
2021-10-13 13:38:08 +0530

    fmt again

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -542,7 +542,9 @@ function Registry(): React.ReactElement {
                                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                   </path>
                                 </svg>
-                                <div>{moduleMeta.star_count}</div>
+                                <div>
+                                  {moduleMeta.star_count}
+                                </div>
                               </div>
                             </>
                           )}
@@ -566,7 +568,9 @@ function Registry(): React.ReactElement {
                               viewBox="0 0 20 20"
                               fill="currentColor"
                             >
-                              <title>Tagged at</title>
+                              <title>
+                                Tagged at
+                              </title>
                               <path
                                 fillRule="evenodd"
                                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
