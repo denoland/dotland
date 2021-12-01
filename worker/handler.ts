b352a3122627c9529ba52edafc099d05c1a3ad35
迷渡 <justjavac@gmail.com>
2021-12-01 10:15:14 +0800

    use cf-proxy
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- worker/handler.ts
+++ worker/handler.ts
@@ -5,7 +5,7 @@ import { handleVSCRequest } from "./vscode.ts";
 import { reportAnalytics } from "./analytics.ts";
 import { ConnInfo } from "https://deno.land/std@0.112.0/http/server.ts";
 
-const REMOTE_URL = "https://cn-proxy.deno.land";
+const REMOTE_URL = "https://cf-proxy.deno.land";
 
 export function withLog(
   handler: (
