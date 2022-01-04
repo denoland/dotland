509b25e22dd244e51f0610d5fec29dd41dd182f2
Kitson Kelly <me@kitsonkelly.com>
2022-01-04 17:43:03 +1100

    feat: add support for v2 registry suggestions
    Closes: #1974

--- worker/main.ts
+++ worker/main.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env -S deno run --allow-read=. --allow-net --allow-env
 /* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
 import { handleRequest, withLog } from "./handler.ts";
@@ -6,4 +7,4 @@ import { listenAndServe } from "https://deno.land/std@0.108.0/http/server.ts";
 const handler = withLog(handleRequest);
 
 console.log("The server is available at http://localhost:8080");
-listenAndServe(":8080", handler);
+listenAndServe(":8081", handler);
