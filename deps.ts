0721b5cb0d38d182103ce4920ab5427287e4e779
crowlkats <crowlkats@toaxl.com>
2022-03-01 12:27:00 +0100

    pin esm.sh version

--- deps.ts
+++ deps.ts
@@ -1,15 +1,16 @@
 export * from "https://raw.githubusercontent.com/piscisaureus/fresh/d990a0bd6b76b2c2ec4016e9bf2584c7c5c8fe88/runtime.ts";
-export { default as twas } from "https://esm.sh/twas@2.1.2";
-export { create } from "https://esm.sh/twind@0.16.16";
-export { css } from "https://esm.sh/twind@0.16.16/css";
+export { default as twas } from "https://esm.sh/twas@2.1.2?pin=v66";
+export { create } from "https://esm.sh/twind@0.16.16?pin=v66";
+export { css } from "https://esm.sh/twind@0.16.16/css?pin=v66";
 export {
   apply,
   shim,
   theme,
   virtualSheet,
-} from "https://esm.sh/twind@0.16.16/shim/server";
+} from "https://esm.sh/twind@0.16.16/shim/server?pin=v66";
 
-export { default as Prism } from "https://esm.sh/prismjs@1.25.0";
-export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
+export { default as Prism } from "https://esm.sh/prismjs@1.25.0?pin=v66";
+export { escape as htmlEscape } from "https://esm.sh/he@1.2.0?pin=v66";
 export { render as gfm } from "https://deno.land/x/gfm@0.1.18/mod.ts";
 export { emojify } from "https://deno.land/x/emoji@0.1.2/mod.ts";
+export { default as compareVersions } from "https://esm.sh/tiny-version-compare@3.0.1?pin=v66";
