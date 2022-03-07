4cd8a9e26ef11c1a195ecceeba4716b109a24d46
Luca Casonato <lucacasonato@yahoo.com>
2022-03-07 15:39:53 +0100

    Revert "refactor: merge worker into fresh (#2051)"
    This reverts commit 37e78f429c5093ba9d9f3e645e6d537b02a038c0.

--- worker/main.ts
+++ worker/main.ts
@@ -1,3 +1,10 @@
-// Copyright 2022 the Deno authors. All rights reserved. MIT license.
+#!/usr/bin/env -S deno run --allow-read=. --allow-net --allow-env
+/* Copyright 2020 the Deno authors. All rights reserved. MIT license. */
 
-import "../main.ts";
+import { handleRequest, withLog } from "./handler.ts";
+import { listenAndServe } from "https://deno.land/std@0.108.0/http/server.ts";
+
+const handler = withLog(handleRequest);
+
+console.log("The server is available at http://localhost:8081");
+listenAndServe(":8081", handler);
