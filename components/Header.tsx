1e14b092889501aabbf6597a3924bef7eb605fe8
justjavac <justjavac@gmail.com>
2022-03-19 12:04:48 +0800

    docs(en): resove all conflicts

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,14 @@
 import { h } from "../deps.ts";
 
 const entries = [
-  { href: "/manual", content: "Manual" },
-  { href: "https://deno.com/blog", content: "Blog" },
+  { href: "/manual", content: "参考手册" },
+  { href: "https://deno.com/blog", content: "博客" },
   {
     href: "https://doc.deno.land/deno/stable",
     content: "API",
   },
-  { href: "/std", content: "Standard Library" },
-  { href: "/x", content: "Third Party Modules" },
+  { href: "/std", content: "标准库" },
+  { href: "/x", content: "第三方模块" },
 ] as const;
 
 export function Header({
@@ -94,19 +94,6 @@ export function Header({
                 </label>
               </div>
               <div class="px-2 pt-4 pb-3">
-<<<<<<< HEAD
-                {[
-                  { href: "https://deno.com/deploy", content: "Deploy" },
-                  { href: "/manual", content: "Manual" },
-                  { href: "https://deno.com/blog", content: "Blog" },
-                  {
-                    href: "https://doc.deno.js.cn/deno/stable",
-                    content: "API",
-                  },
-                  { href: "/std", content: "Standard Library" },
-                  { href: "/x", content: "Third Party Modules" },
-                ].map(({ href, content }) => (
-=======
                 <a
                   href="https://deno.com/deploy"
                   class="block px-3 py-2 rounded-md text-base font-medium rounded-lg border-2 border-gray-900 bg-gray-900 text-gray-200 hover:border-gray-700 hover:bg-transparent hover:text-gray-900 focus:(outline-none text-gray-900 bg-gray-50) transition duration-150 ease-in-out"
@@ -114,7 +101,6 @@ export function Header({
                   Deploy
                 </a>
                 {entries.map(({ href, content }) => (
->>>>>>> 6914e63e6c636e33199080ad0ebe53fecb72b8fa
                   <a
                     href={href}
                     class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:(text-gray-900 bg-gray-50) focus:(outline-none text-gray-900 bg-gray-50) transition duration-150 ease-in-out"
@@ -154,7 +140,6 @@ export function Header({
           >
             Deploy
           </a>
-<<<<<<< HEAD
           <a
             href="/manual"
             class="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
@@ -185,16 +170,6 @@ export function Header({
           >
             第三方模块
           </a>
-=======
-          {entries.map(({ href, content }) => (
-            <a
-              href={href}
-              class="ml-10 my-auto font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
-            >
-              {content}
-            </a>
-          ))}
->>>>>>> 6914e63e6c636e33199080ad0ebe53fecb72b8fa
           <a
             href="https://github.com/denoland"
             class="ml-10 my-auto text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out leading-0"
