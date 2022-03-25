0feba0bfd4f505ba6534c7a9faa9304c3d063e26
Yongwook Choi <hyperflow@kakao.com>
2022-03-25 15:37:09 +0900

    refactor: import from server_deps (#2089)

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/04a32f34987fbd59ce646967f216b2cca072a58f/server.ts";
+import { start } from "./server_deps.ts";
 import routes from "./routes.gen.ts";
 
 await start(routes);
