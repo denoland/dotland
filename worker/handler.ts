40c9c987281e0fc406aa59005e60e5b950d4e9a1
Luca Casonato <hello@lcas.dev>
2022-04-11 13:36:50 +0200

    fix: don't cache in dotland worker (#2093)

--- worker/handler.ts
+++ worker/handler.ts
@@ -173,7 +173,7 @@ export function handleRequest(request: Request): Promise<Response> {
     return Promise.resolve(new Response(null, { status: 405 })); // Method not allowed.
   }
 
-  return proxyFile(url, REMOTE_URL, request);
+  return proxyFile(url, REMOTE_URL);
 }
 
 const ALT_LINENUMBER_MATCHER = /(.*):(\d+):\d+$/;
@@ -189,81 +189,13 @@ export function extractAltLineNumberReference(
   };
 }
 
-interface CacheEntry {
-  body: ArrayBuffer;
-  contentType: string;
-  etag: string;
-  immutable: boolean;
-}
-
-const cache = new Map<string, CacheEntry>();
-
-// When deploying a new version, the worker will be updated before the static
-// website hosted by Vercel gets updated. Therefore we clear the cache 2 minutes
-// after startup up to ensure that there is no stale static content in the
-// in-memory cache.
-setTimeout(() => cache.clear(), 2 * 60 * 1000);
-
-async function proxyFile(
-  url: URL,
-  remoteUrl: string,
-  request: Request,
-): Promise<Response> {
+async function proxyFile(url: URL, remoteUrl: string): Promise<Response> {
   const proxyUrl = new URL(remoteUrl + url.pathname + url.search).href;
-  let cacheEntry = cache.get(proxyUrl);
 
-  if (cacheEntry === undefined) {
-    const proxyRequest = new Request(proxyUrl);
-    const proxyResponse = await fetchWithRetry(proxyRequest);
+  const proxyRequest = new Request(proxyUrl);
+  const proxyResponse = await fetchWithRetry(proxyRequest);
 
-    if (!(proxyResponse.ok || proxyResponse.redirected)) {
-      return proxyResponse;
-    }
-
-    const body = await proxyResponse.arrayBuffer();
-    const contentType = proxyResponse.headers.get("content-type") ??
-      "application/binary";
-    const etag = await crypto.subtle.digest("SHA-1", body).then((hash) =>
-      Array.from(new Uint8Array(hash))
-        .map((byte) => byte.toString(16).padStart(2, "0"))
-        .join("")
-    );
-    const immutable = /\bimmutable\b/i
-      .test(proxyResponse.headers.get("cache-control") ?? "");
-
-    cacheEntry = {
-      body,
-      contentType,
-      etag,
-      immutable,
-    };
-    cache.set(proxyUrl, cacheEntry);
-  }
-
-  if (request.headers.get("if-none-match") !== cacheEntry.etag) {
-    let body;
-    switch (request.method) {
-      case "HEAD":
-        body = null;
-        break;
-      case "GET":
-        body = cacheEntry.body;
-        break;
-      default:
-        throw new Error(`Unsupported request method: ${request.method}`);
-    }
-    return new Response(body, {
-      headers: {
-        "content-type": cacheEntry.contentType,
-        "cache-control": cacheEntry.immutable
-          ? "public,max-age=31536000,immutable"
-          : "public,max-age=0,must-revalidate",
-        "etag": cacheEntry.etag,
-      },
-    });
-  } else {
-    return new Response(null, { status: 304 }); // Not modified.
-  }
+  return proxyResponse;
 }
 
 async function fetchWithRetry(request: Request): Promise<Response> {
