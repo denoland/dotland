7b2845295b33f9fa4a474cd7a458e4a11fc4851c
Ryan Dahl <ry@tinyclouds.org>
2022-04-21 00:19:43 -0400

    fmt

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,8 @@ import versions from "../versions.json" assert { type: "json" };
 import { Background } from "../components/HeroBackground.tsx";
 
 export default function Home() {
-  const complexExampleProgram = `import { serve } from "https://deno.land/std/http/server.ts";
+  const complexExampleProgram =
+    `import { serve } from "https://deno.land/std/http/server.ts";
 serve(req => new Response("Hello World\\n"));`;
 
   const denoTestExample =
