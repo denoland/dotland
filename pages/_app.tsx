d42ba41509d5c260f2cc9649d6356a9292e291f5
lambtron <andyjiang@gmail.com>
2022-05-02 10:36:34 -0700

    add full path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,7 @@ export default function App({ Component }) {
           content="Deno is a simple, modern runtime for JavaScript and
             TypeScript that uses V8 and is built in Rust."
         />
-        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:image" content="https://deno.land/og-image.png" />
         <meta property="og:type" content="website" />
         <meta name="robots" content="index, follow" />
         <meta
