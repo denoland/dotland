e1de11740dbe31ee48d77443fa57b3aee7f0a9db
Andy Jiang <andyjiang@gmail.com>
2022-06-08 14:46:23 -0400

    Add hello bar (#2170)
    * add a hellobar
    
    * made entire bar clickable
    
    * Update index.tsx
    
    Co-authored-by: Ryan Dahl <ry@tinyclouds.org>

---
+++ components/HelloBar.tsx
@@ -0,0 +1,17 @@
+// Copyright 2022 the Deno authors. All rights reserved. MIT license.
+
+/** @jsx h */
+import { ComponentChildren, h, tw } from "../deps.ts";
+
+export function HelloBar(props: {
+  to: string;
+  children: ComponentChildren;
+}) {
+  return (
+    <div class={tw`text-center bg-black text-white`}>
+      <a href={props.to} target="_blank" class={tw`h-full w-full block p-2`}>
+        {props.children}
+      </a>
+    </div>
+  );
+}
