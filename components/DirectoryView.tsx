a00272621abbc71df86da580941bb8158267e28e
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-13 19:40:53 +0200

    factor out icons

--- components/DirectoryView.tsx
+++ components/DirectoryView.tsx
@@ -2,6 +2,7 @@
 import { h } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 import { DirListing, Entry, isReadme } from "@/util/registry_utils.ts";
+import * as Icons from "@/components/Icons.tsx";
 
 export function DirectoryView(props: {
   dirListing: DirListing[];
@@ -156,32 +157,17 @@ function TableRow({
           }`}
           tabIndex={-1}
         >
-          <svg fill="currentColor" viewBox="0 0 20 20" class={tw`w-5 h-5`}>
-            {(() => {
-              switch (entry.type) {
-                case "file":
-                  if (isReadme(entry.name)) {
-                    return (
-                      <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z">
-                      </path>
-                    );
-                  }
-                  return (
-                    <path
-                      fillRule="evenodd"
-                      d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
-                      clipRule="evenodd"
-                    >
-                    </path>
-                  );
-                case "dir":
-                  return (
-                    <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z">
-                    </path>
-                  );
-              }
-            })()}
-          </svg>
+          {(() => {
+            switch (entry.type) {
+              case "file":
+                if (isReadme(entry.name)) {
+                  return <Icons.OpenBook />;
+                }
+                return <Icons.Page />;
+              case "dir":
+                return <Icons.Folder />;
+            }
+          })()}
         </a>
       </td>
       <td class={tw`whitespace-no-wrap text-sm text-blue-500 leading-5`}>
