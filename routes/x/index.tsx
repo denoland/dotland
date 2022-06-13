9ce12938aa269d904df95b96bb08caa7c2361cd3
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-13 18:54:31 +0200

    refactor: Add Icons components

--- routes/x/index.tsx
+++ routes/x/index.tsx
@@ -426,17 +426,7 @@ function ModuleList({
                   </div>
                 )}
                 <div>
-                  <svg
-                    class={tw`h-5 w-5 text-gray-400`}
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <Icons.ArrowRight />
                 </div>
               </div>
             </a>
--- routes/x/module.tsx
+++ routes/x/module.tsx
@@ -34,6 +34,7 @@ import { Footer } from "@/components/Footer.tsx";
 import { FileDisplay } from "@/components/FileDisplay.tsx";
 import { DirectoryListing } from "@/components/DirectoryListing.tsx";
 import { ErrorMessage } from "@/components/ErrorMessage.tsx";
+import * as Icons from "@/components/Icons.tsx";
 import VersionSelect from "@/islands/VersionSelect.tsx";
 
 // 100kb
@@ -212,18 +213,7 @@ function ModuleView({
                     >
                     </div>
                     <div class={tw`mt-3 flex items-center py-0.5`}>
-                      <svg
-                        class={tw`h-5 w-5 mr-2 inline text-gray-200`}
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <title>GitHub Repository</title>
-                        <path
-                          fillRule="evenodd"
-                          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <Icons.Github class="mr-2 inline text-gray-200" />
                       <div class={tw`w-4/5 sm:w-2/3 bg-gray-100 h-4`}></div>
                     </div>
                     <div class={tw`mt-2 flex items-center py-0.5`}>
@@ -250,15 +240,7 @@ function ModuleView({
                     {moduleDocumentationURL
                       ? (
                         <div class={tw`mt-3 flex items-center`}>
-                          <svg
-                            class={tw`h-5 w-5 mr-2 inline text-gray-700`}
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <title>Documentation</title>
-                            <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z">
-                            </path>
-                          </svg>
+                          <Icons.OpenBook class="mr-2 inline text-gray-700" />
                           <a class={tw`link`} href={moduleDocumentationURL}>
                             Documentation
                           </a>
@@ -266,18 +248,7 @@ function ModuleView({
                       )
                       : null}
                     <div class={tw`mt-3 flex items-center`}>
-                      <svg
-                        class={tw`h-5 w-5 mr-2 inline text-gray-700`}
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <title>GitHub Repository</title>
-                        <path
-                          fillRule="evenodd"
-                          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <Icons.GitHub class="mr-2 inline text-gray-700" />
                       <a
                         class={tw`link`}
                         href={`https://github.com/${versionMeta.uploadOptions.repository}`}
