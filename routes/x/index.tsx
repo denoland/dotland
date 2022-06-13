a77f83eb82635b34d5cd9b12f1ed9699ace28278
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-13 19:34:00 +0200

    refactor: Add icons components (#2179)

--- routes/x/index.tsx
+++ routes/x/index.tsx
@@ -11,6 +11,7 @@ import { emojify } from "$emoji";
 import { Header } from "@/components/Header.tsx";
 import { Footer } from "@/components/Footer.tsx";
 import { InlineCode } from "@/components/InlineCode.tsx";
+import * as Icons from "@/components/Icons.tsx";
 
 import {
   getStats,
@@ -412,31 +413,11 @@ function ModuleList({
                     <div class={tw`text-gray-400`}>
                       {meta.starCount}
                     </div>
-                    <svg
-                      class={tw`ml-1 text-gray-400 w-5 h-5`}
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <title>
-                        star
-                      </title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                      </path>
-                    </svg>
+                    <Icons.Star title="star" />
                   </div>
                 )}
                 <div>
-                  <svg
-                    class={tw`h-5 w-5 text-gray-400`}
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <Icons.ArrowRight />
                 </div>
               </div>
             </a>
