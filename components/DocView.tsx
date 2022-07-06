54203e5025eb0b7537b621b42288bc223d5ac7d4
Leo Kettmeir <crowlkats@toaxl.com>
2022-07-06 12:04:00 +0200

    fix: add code view button (#2241)

--- components/DocView.tsx
+++ components/DocView.tsx
@@ -5,12 +5,13 @@
 import { Fragment, h } from "preact";
 import { tw } from "@twind";
 import { type CommonProps, getBasePath } from "@/util/registry_utils.ts";
-import { Doc } from "@/util/doc.ts";
+import { type Doc, type DocNode } from "@/util/doc.ts";
 import { dirname } from "$std/path/mod.ts";
 import { ModuleDoc } from "$doc_components/module_doc.tsx";
 import { ModulePathIndex } from "$doc_components/module_path_index.tsx";
 import { ModulePathIndexPanel } from "$doc_components/module_path_index_panel.tsx";
 import { FileDisplay } from "./FileDisplay.tsx";
+import * as Icons from "./Icons.tsx";
 
 export function DocView({
   doc,
@@ -46,26 +47,30 @@ export function DocView({
       <div class={tw`space-y-12 flex flex-col gap-4 w-full overflow-auto`}>
         {doc
           ? (
-            <div>
-              <ModuleDoc url={url.href}>
-                {doc}
-              </ModuleDoc>
-            </div>
+            <FileDoc url={url.href} sourceURL={baseURL + path}>
+              {doc}
+            </FileDoc>
           )
           : (
             <div class={tw`space-y-12`}>
-              <ModulePathIndex
-                base={basePath}
-                path={path || "/"}
-                skipMods={!!index.indexModule}
-              >
-                {index.index}
-              </ModulePathIndex>
-              {index.indexModule && (
-                <ModuleDoc url={`${baseURL}${index.indexModule}`}>
-                  {index.nodes}
-                </ModuleDoc>
-              )}
+              {index.indexModule
+                ? (
+                  <FileDoc
+                    url={baseURL + index.indexModule}
+                    sourceURL={baseURL + path}
+                  >
+                    {index.nodes}
+                  </FileDoc>
+                )
+                : (
+                  <ModulePathIndex
+                    base={basePath}
+                    path={path || "/"}
+                    skipMods={!!index.indexModule}
+                  >
+                    {index.index}
+                  </ModulePathIndex>
+                )}
             </div>
           )}
 
@@ -83,5 +88,32 @@ export function DocView({
         )}
       </div>
     </>
+  );
+}
+
+function FileDoc({
+  children,
+  url,
+  sourceURL,
+}: {
+  sourceURL: string;
+  children: DocNode[];
+  url: string;
+}) {
+  return (
+    <div>
+      <div class={tw`flex justify-between mb-8`}>
+        <div>{/* TODO: add module name */}</div>
+        <a
+          href={`${sourceURL}?code`}
+          class={tw`rounded-md border border-dark-border p-2`}
+        >
+          <Icons.SourceFile />
+        </a>
+      </div>
+      <ModuleDoc url={url}>
+        {children}
+      </ModuleDoc>
+    </div>
   );
 }
