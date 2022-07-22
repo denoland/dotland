1ee6fe6bd6fd2935923dcea14a056e1870731d48
crowlkats <crowlkats@toaxl.com>
2022-07-22 22:01:14 +0200

    fix: handle ctrl+k and /

--- islands/GlobalSearch.tsx
+++ islands/GlobalSearch.tsx
@@ -70,7 +70,7 @@ export default function GlobalSearch() {
 
   useEffect(() => {
     const keyboardHandler = (e: KeyboardEvent) => {
-      if (e.metaKey && e.key === "k") {
+      if (((e.metaKey || e.ctrlKey) && e.key === "k") || e.key === "/") {
         e.preventDefault();
         setShowModal(true);
       }
