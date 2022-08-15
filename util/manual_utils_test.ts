40e4896e2d8b51e60de8f72f3e354885ae44c03e
迷渡 <justjavac@gmail.com>
2022-08-15 08:19:44 +0800

    doc: resove all conflicts
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- util/manual_utils_test.ts
+++ util/manual_utils_test.ts
@@ -17,29 +17,14 @@ Deno.test("get introduction file commit hash", () => {
 Deno.test("get introduction file old repo", () => {
   assertEquals(
     getFileURL("v1.12.0", "/introduction"),
-<<<<<<< HEAD
     "https://cdn.jsdelivr.net/gh/denocn/deno_docs@master/introduction.md",
-=======
-    "http://deno-registry2-prod-storagebucket-b3a31d16.s3-website-us-east-1.amazonaws.com/deno/versions/v1.12.0/raw/docs/introduction.md",
->>>>>>> c6f8777fa15fe69e5cb120f86a7a8cdaed43b9e0
   );
 });
 
 Deno.test("get introduction file new repo", () => {
   assertEquals(
     getFileURL("v1.12.1", "/introduction"),
-<<<<<<< HEAD
     "https://cdn.jsdelivr.net/gh/denocn/deno_docs@master/introduction.md",
-=======
-    "http://deno-registry2-prod-storagebucket-b3a31d16.s3-website-us-east-1.amazonaws.com/manual/versions/v1.12.1/raw/introduction.md",
-  );
-});
-
-Deno.test("get edit link old repo", () => {
-  assertEquals(
-    getDocURL("v1.12.0", "/introduction"),
-    "https://github.com/denoland/deno/blob/v1.12.0/docs/introduction.md",
->>>>>>> c6f8777fa15fe69e5cb120f86a7a8cdaed43b9e0
   );
 });
 
