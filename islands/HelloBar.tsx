d6ae2c4b3909323eeaead97b602df62e0bcee990
Leo Kettmeir <crowlkats@toaxl.com>
2022-08-30 01:50:38 +0200

    refactor: simplify HelloBar (#2422)

--- islands/HelloBar.tsx
+++ islands/HelloBar.tsx
@@ -1,9 +1,8 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
 /** @jsx h */
-/** @jsxFrag Fragment */
-import { ComponentChildren, Fragment, h } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { ComponentChildren, h } from "preact";
+import { useState } from "preact/hooks";
 import { tw } from "@twind";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Cross } from "@/components/Icons.tsx";
@@ -12,40 +11,36 @@ export default function HelloBar(props: {
   to: string;
   children: ComponentChildren;
 }) {
-  const [helloBar, setHelloBar] = useState(false);
-
-  useEffect(() => {
-    setHelloBar(localStorage.getItem("helloBar") != props.to);
-  }, []);
+  const [helloBar, setHelloBar] = useState(
+    IS_BROWSER ? (localStorage.getItem("helloBar") != props.to) : false,
+  );
 
   return (
     <div>
-      {helloBar
-        ? (
-          <div
-            class={tw`text-center bg-black text-white p-1 flex items-center justify-between flex-wrap`}
-          >
-            <div class={tw`flex-grow text-center`}>
-              <a
-                href={props.to}
-                target="_blank"
-                class={tw`inline-block p-1 hover:text-underline`}
-              >
-                {props.children}
-              </a>
-            </div>
-            <button
-              disabled={!IS_BROWSER}
-              onClick={() => {
-                localStorage.setItem("helloBar", props.to);
-                setHelloBar(false);
-              }}
+      {helloBar && (
+        <div
+          class={tw`text-center bg-black text-white p-1 flex items-center justify-between flex-wrap`}
+        >
+          <div class={tw`flex-grow text-center`}>
+            <a
+              href={props.to}
+              target="_blank"
+              class={tw`inline-block p-1 hover:text-underline`}
             >
-              <Cross />
-            </button>
+              {props.children}
+            </a>
           </div>
-        )
-        : <></>}
+          <button
+            disabled={!IS_BROWSER}
+            onClick={() => {
+              localStorage.setItem("helloBar", props.to);
+              setHelloBar(false);
+            }}
+          >
+            <Cross />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
