7ca339ca7914d9b9607b74f1b182f886300c1ae3
Leo Kettmeir <crowlkats@toaxl.com>
2022-09-05 13:51:13 +0100

    chore: update doc components (#2443)

--- components/DocView.tsx
+++ components/DocView.tsx
@@ -6,10 +6,10 @@ import { Fragment, h } from "preact";
 import { tw } from "@twind";
 import { type CommonProps, getModulePath } from "@/util/registry_utils.ts";
 import { dirname } from "$std/path/mod.ts";
-import { ModuleDoc } from "$doc_components/module_doc.tsx";
-import { ModulePathIndex } from "$doc_components/module_path_index.tsx";
-import { ModulePathIndexPanel } from "$doc_components/module_path_index_panel.tsx";
-import { SymbolDoc } from "$doc_components/symbol_doc.tsx";
+import { ModuleDoc } from "$doc_components/doc/module_doc.tsx";
+import { ModuleIndex } from "$doc_components/doc/module_index.tsx";
+import { ModuleIndexPanel } from "$doc_components/doc/module_index_panel.tsx";
+import { SymbolDoc } from "$doc_components/doc/symbol_doc.tsx";
 import {
   DocPageIndex,
   DocPageModule,
@@ -32,14 +32,14 @@ export function DocView({
     <SidePanelPage
       sidepanel={(data.kind === "module" || data.kind === "symbol")
         ? (
-          <ModulePathIndexPanel
+          <ModuleIndexPanel
             base={basePath}
             path={dirname(path)}
             current={path}
             currentSymbol={data.kind === "symbol" ? data.name : undefined}
           >
             {data.nav}
-          </ModulePathIndexPanel>
+          </ModuleIndexPanel>
         )
         : null}
     >
@@ -48,13 +48,13 @@ export function DocView({
           switch (data.kind) {
             case "index":
               return (
-                <ModulePathIndex
+                <ModuleIndex
                   base={basePath}
                   path={path || "/"}
                   sourceUrl={url.href}
                 >
                   {data.items}
-                </ModulePathIndex>
+                </ModuleIndex>
               );
             case "symbol":
               return (
