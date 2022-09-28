a5aee453473a8137224f5bf63e23d23b4739bc9b
Kitson Kelly <me@kitsonkelly.com>
2022-09-29 07:47:52 +1000

    feat: improve social media images

--- components/ContentMeta.tsx
+++ components/ContentMeta.tsx
@@ -18,11 +18,28 @@ const DEFAULT_KEYWORDS = [
 type OgType = "website" | "article";
 
 const OgImagePath = {
-  "generic": "https://deno.land/og-image.png",
-  "manual": "https://deno.land/og-manual.png",
-  "api": "https://deno.land/og-api.png",
-  "std": "https://deno.land/og-std.png",
-  "modules": "https://deno.land/og-modules.png",
+  "generic": {
+    img: "https://deno.land/og-image.png",
+    alt: "A logo of a sauropod in the rain and the word Deno",
+  },
+  "manual": {
+    img: "https://deno.land/og-manual.png",
+    alt: "A logo of a sauropod reading a book and the title Deno Manual",
+  },
+  "api": {
+    img: "https://deno.land/og-api.png",
+    alt: "A logo of a sauropod reading a book and the title Deno API Reference",
+  },
+  "std": {
+    img: "https://deno.land/og-std.png",
+    alt:
+      "A logo of a sauropod holding a stack of books and the title Deno Standard Library",
+  },
+  "modules": {
+    img: "https://deno.land/og-modules.png",
+    alt:
+      "A logo of a sauropod looking at some blocks on a table and the title Deno Modules",
+  },
 };
 type OgImageType = keyof typeof OgImagePath;
 
@@ -54,30 +71,34 @@ export function ContentMeta(
   }
   return (
     <Head>
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site:id" content="@deno_land" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@deno_land" />
 
       <title>{title}</title>
       <meta name="twitter:title" content={title} />
       <meta property="og:title" content={title} />
 
-      {creator && <meta name="twitter:creator:id" content={creator} />}
+      {description && (
+        <>
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+          <meta name="description" content={description} />
+        </>
+      )}
+
+      {creator && <meta name="twitter:creator" content={creator} />}
+
+      <meta name="twitter:image" content={OgImagePath[ogImage].img} />
+      <meta name="twitter:image:alt" content={OgImagePath[ogImage].alt} />
+      <meta property="og:image" content={OgImagePath[ogImage].img} />
+      <meta property="og:image:alt" content={OgImagePath[ogImage].alt} />
 
       <meta property="og:type" content={ogType} />
       <meta property="og:site_name" content="Deno" />
       <meta property="og:locale" content="en_US" />
-      <meta property="og:image" content={OgImagePath[ogImage]} />
 
       <meta name="robots" content={noIndex ? "noindex" : "index, follow"} />
       <meta name="keywords" content={keywords.join(", ")} />
-
-      {description && (
-        <>
-          <meta name="twitter:description" content={description} />
-          <meta property="og:description" content={description} />
-          <meta name="description" content={description} />
-        </>
-      )}
     </Head>
   );
 }
