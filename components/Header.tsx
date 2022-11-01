b3f913368fe3ee83e1a1f8748642bebeb4c400a0
crowlkats <crowlkats@toaxl.com>
2022-11-01 06:37:48 +0100

    header refactor

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,11 +7,22 @@ import GlobalSearch from "@/islands/GlobalSearch.tsx";
 import versions from "@/versions.json" assert { type: "json" };
 
 const entries = [
-  { href: "/manual", content: "Manual" },
-  { href: "/api", content: "API" },
-  { href: "/std", content: "Standard Library" },
-  { href: "/x", content: "Third Party Modules" },
-] as const;
+  {
+    content: "Modules",
+    children: [
+      { href: "/std", content: "Standard Library" },
+      { href: "/x", content: "Third Party Modules" },
+    ],
+  },
+  {
+    content: "Docs",
+    children: [
+      { href: "/manual", content: "Manual" },
+      { href: "/api", content: "API" },
+    ],
+  },
+  { href: "https://deno.com/deploy", content: "Deploy" },
+];
 
 type ContentTypes = (typeof entries)[number]["content"];
 
@@ -31,18 +42,10 @@ export function Header({ selected, manual }: {
           />
 
           <div class="h-9 flex flex-1 items-center justify-between lg:justify-start select-none w-full lg:w-min gap-3 md:gap-6 lg:gap-8">
-            <a
-              href="/"
-              class={tw`h-8 w-8 block ${
-                css({
-                  "flex-shrink": "0",
-                })
-              }`}
-            >
-              <img class="h-full w-full" src="/logo.svg" alt="Deno Logo" />
+            <a href="/" class="flex items-center flex-none gap-4">
+              <Icons.Logo class="h-8 flex-none" />
+              <Icons.Deno class="h-6 flex-none" />
             </a>
-
-            <GlobalSearch denoVersion={versions.cli[0]} />
 
             <label
               tabIndex={0}
@@ -60,41 +63,44 @@ export function Header({ selected, manual }: {
             </label>
           </div>
 
-          <div class="hidden flex-col mx-2 mt-5 gap-y-4 lg:(flex flex-row items-center mx-0 mt-0) font-medium">
-            {entries.map(({ href, content }) => (
-              <a
-                href={href}
-                class={tw`lg:ml-4 px-2 rounded-md leading-loose hover:(bg-grayDefault) ${apply`${
-                  content === selected
-                    ? css({
-                      "text-decoration-line": "underline",
-                      "text-underline-offset": "6px",
-                      "text-decoration-thickness": "2px",
-                    })
-                    : ""
-                } ${content === selected ? "text-black" : "text-gray-500"}`}`}
-              >
-                {content}
-              </a>
-            ))}
+          <div class="hidden flex-col mx-2 mt-5 gap-x-5 gap-y-4 font-medium lg:(flex flex-row items-center mx-0 mt-0)">
+            <div class="space-x-3.5 select-none">
+              {entries.map(({ href, content, children }) => (
+                <div
+                  class={`inline-block leading-loose children:first-child:(block px-2 rounded-t-md ${
+                    !children ? "rounded-b-md" : ""
+                  }) hover:children:(bg-grayDefault even:block) text-gray-500`}
+                >
+                  {href ? <a href={href}>{content}</a> : <span>{content}</span>}
 
-            <a
-              href="https://deno.com/deploy"
-              class="button-outline lg:ml-5"
-            >
-              Deploy
-            </a>
+                  {children && (
+                    <div class="hidden rounded-b-md rounded-tr-md absolute bottom overflow-hidden">
+                      {children.map(({
+                        href,
+                        content,
+                      }) => (
+                        <a class="block px-3 hover:bg-border" href={href}>
+                          {content}
+                        </a>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <GlobalSearch denoVersion={versions.cli[0]} />
 
             <a
               href="https://github.com/denoland/deno"
-              class="lg:ml-5 my-auto hidden lg:block"
+              class="my-auto hidden lg:block"
             >
               <span class="sr-only">GitHub</span>
               <Icons.GitHub class="h-5 w-auto text-gray-600 hover:text-black" />
             </a>
             <a
               href="https://discord.gg/deno"
-              class="lg:ml-5 my-auto hidden lg:block"
+              class="my-auto hidden lg:block"
             >
               <span class="sr-only">Discord</span>
               <Icons.Discord class="h-5 w-auto text-gray-600 hover:text-black" />
