830c069e8db37fb1a1ae0d2f4001b132c8761f39
reaper <ahoy@barelyhuman.dev>
2022-11-10 10:48:34 +0530

    fmt fix

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,9 +14,9 @@ export default function App({ Component }: AppProps) {
 
         {/* needed for the dialog on safari to handle visibility of modal based on the `open` prop */}
         <link
-            rel="stylesheet"
-            href="https://esm.sh/dialog-polyfill@0.5.6/dialog-polyfill.css"
-          />
+          rel="stylesheet"
+          href="https://esm.sh/dialog-polyfill@0.5.6/dialog-polyfill.css"
+        />
 
         <link rel="stylesheet" href="/app.css" />
         <link rel="stylesheet" href="/gfm.css" />
