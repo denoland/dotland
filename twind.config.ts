b4f1b7ee52fbfc68bc144d9ed53a7de44703210a
crowlkats <crowlkats@toaxl.com>
2022-11-30 23:14:24 +0100

    fix: fix LP title width and add more padding for larger screens on all pages

--- twind.config.ts
+++ twind.config.ts
@@ -70,7 +70,9 @@ export default {
     "section-x-inset": (parts) =>
       parts[0] === "none"
         ? apply`max-w-none mx-0 px-0`
-        : apply`max-w-screen-${parts[0]} mx-auto px-6 md:px-8`,
+        : apply`max-w-screen-${
+          parts[0]
+        } mx-auto px-6 md:px-8 lg:px-10 xl:px-14`,
     "form-select-bg": css({
       "background-image":
         `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='none'%3e%3cpath d='M7 7l3-3 3 3m0 6l-3 3-3-3' stroke='%239fa6b2' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3e%3c/svg%3e")`,
