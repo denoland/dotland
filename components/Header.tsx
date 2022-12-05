cfa36d0f8ce2c3e04fe3a9b2546dfd653a1dd73a
lambtron <andyjiang@gmail.com>
2022-12-05 11:23:57 -0800

    revert lockfile

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,11 +57,6 @@ const entries: Array<HrefEntry | ChildrenEntry> = [
         content: "YouTube",
         icon: Icons.YouTube,
       },
-      {
-        href: "https://fosstodon.org/@deno_land",
-        content: "Mastodon",
-        icon: Icons.Mastodon,
-      },
     ],
   },
 ];
