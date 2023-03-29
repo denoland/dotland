8a4b51487532f8fcb4e4649866f08d2a829c4f9d
hashrock <hashrock@users.noreply.github.com>
2023-03-29 20:50:29 +0900

    remove in-page toc

--- components/SidePanelPage.tsx
+++ components/SidePanelPage.tsx
@@ -45,115 +45,6 @@ export function SidePanelPage({ children, sidepanel }: {
         >
           {children}
         </main>
-
-        <div class="hidden xl:sticky xl:top-[4.5rem] xl:-mr-6 xl:block xl:h-[calc(100vh-4.5rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6">
-          <nav aria-labelledby="on-this-page-title" class="w-56">
-            <h2
-              id="on-this-page-title"
-              class="font-display text-sm font-medium text-slate-900 dark:text-white"
-            >
-              On this page
-            </h2>
-            <ol role="list" class="mt-4 space-y-3 text-sm">
-              <li>
-                <h3>
-                  <a class="text-sky-500" href="/#quick-start">Quick start</a>
-                </h3>
-                <ol
-                  role="list"
-                  class="mt-2 space-y-3 pl-5 text-slate-500 dark:text-slate-400"
-                >
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#installing-dependencies"
-                    >
-                      Installing dependencies
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#configuring-the-library"
-                    >
-                      Configuring the library
-                    </a>
-                  </li>
-                </ol>
-              </li>
-              <li>
-                <h3>
-                  <a
-                    class="font-normal text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300"
-                    href="/#basic-usage"
-                  >
-                    Basic usage
-                  </a>
-                </h3>
-                <ol
-                  role="list"
-                  class="mt-2 space-y-3 pl-5 text-slate-500 dark:text-slate-400"
-                >
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#your-first-cache"
-                    >
-                      Your first cache
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#clearing-the-cache"
-                    >
-                      Clearing the cache
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#adding-middleware"
-                    >
-                      Adding middleware
-                    </a>
-                  </li>
-                </ol>
-              </li>
-              <li>
-                <h3>
-                  <a
-                    class="font-normal text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300"
-                    href="/#getting-help"
-                  >
-                    Getting help
-                  </a>
-                </h3>
-                <ol
-                  role="list"
-                  class="mt-2 space-y-3 pl-5 text-slate-500 dark:text-slate-400"
-                >
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#submit-an-issue"
-                    >
-                      Submit an issue
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="hover:text-slate-600 dark:hover:text-slate-300"
-                      href="/#join-the-community"
-                    >
-                      Join the community
-                    </a>
-                  </li>
-                </ol>
-              </li>
-            </ol>
-          </nav>
-        </div>
       </div>
     </div>
   );
@@ -175,8 +66,9 @@ export function ManualOrAPI(
           href={`${entries[key]}@${version}`}
         >
           <div
-            class={`pt-2 ${key === current ? "border-b-2 border-black pb-1.5" : "pb-2"
-              }`}
+            class={`pt-2 ${
+              key === current ? "border-b-2 border-black pb-1.5" : "pb-2"
+            }`}
           >
             {key}
           </div>
