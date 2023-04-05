e9688323097638dd700b9c833358f013252ba175
hashrock <hashrock@users.noreply.github.com>
2023-04-05 23:55:17 +0900

    fmt

--- static/manual.js
+++ static/manual.js
@@ -3,7 +3,9 @@ const path = url.pathname;
 
 // deno-lint-ignore no-window-prefix
 window.addEventListener("DOMContentLoaded", (_) => {
-  const el = document.querySelector(`#SidePanelScrollContainer a[href="${path}"]`);
+  const el = document.querySelector(
+    `#SidePanelScrollContainer a[href="${path}"]`,
+  );
   const containerEl = document.querySelector("#SidePanelScrollContainer");
   const containerRect = containerEl.getBoundingClientRect();
 
